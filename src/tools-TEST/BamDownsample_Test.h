2fa3078ec8c1871ec9aa261f6e934b1c4585bb44
Florian Lenz <Florian.Lenz@med.uni-tuebingen.de>
2015-11-27 10:53:29 +0100

    BamDownsample_Test: Separate test for Windows and Linux

--- src/tools-TEST/BamDownsample_Test.h
+++ src/tools-TEST/BamDownsample_Test.h
@@ -8,8 +8,16 @@ private slots:
 
 	void test_01()
 	{
+	#ifdef _WIN32
 		EXECUTE("BamDownsample", "-in " + TESTDATA("data_in/BamDownsample_in1.bam") + " -out out/BamDownsample_out1.bam -percentage 80 -test");
 		IS_TRUE(QFile::exists("out/BamDownsample_out1.bam"));
-		COMPARE_FILES("out/BamDownsample_out1.txt", TESTDATA("data_out/BamDownsample_out1.txt"));
+		COMPARE_FILES("out/BamDownsample_out1.txt", TESTDATA("data_out/BamDownsample_out1_Windows.txt"));
+	#elif __linux__
+		EXECUTE("BamDownsample", "-in " + TESTDATA("data_in/BamDownsample_in1.bam") + " -out out/BamDownsample_out1.bam -percentage 80 -test");
+		IS_TRUE(QFile::exists("out/BamDownsample_out1.bam"));
+		COMPARE_FILES("out/BamDownsample_out1.txt", TESTDATA("data_out/BamDownsample_out1_Linux.txt"));
+	#else
+		SKIP("No testcase for this OS (only Windows or Linux");
+	#endif
 	}
 };
