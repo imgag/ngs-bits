imgag/ngs-bits

commit de762b55f701d9cc46f47b0ac2b394a2b01c5c4b

SeqPurge: added error correction (optional)

src/SeqPurge/AnalysisWorker.cpp
@@ -5,12 +5,13 @@
 
 QVector<double> AnalysisWorker::fak_cache = QVector<double>();
 
-AnalysisWorker::AnalysisWorker(QSharedPointer<FastqEntry> e1, QSharedPointer<FastqEntry> e2, TrimmingParameters& params, TrimmingStatistics& stats, TrimmingData& data)
+AnalysisWorker::AnalysisWorker(QSharedPointer<FastqEntry> e1, QSharedPointer<FastqEntry> e2, TrimmingParameters& params, TrimmingStatistics& stats, ErrorCorrectionStatistics& ecstats, TrimmingData& data)
     : QRunnable()
 	, e1_(e1)
 	, e2_(e2)
 	, params_(params)
 	, stats_(stats)
+	, ecstats_(ecstats)
 	, data_(data)
 {
 }
@@ -73,9 +74,67 @@ double AnalysisWorker::matchProbability(int matches, int mismatches)
 	return p;
 }
 
+void AnalysisWorker::correctErrors(QTextStream& debug_out)
+{
+	int mm_count = 0;
+	const int count = e1_->bases.count();
+	for (int i=0; i<count; ++i)
+	{
+		const int i2 = count-i-1;
+
+		//error detected
+		if (e1_->bases[i]!=NGSHelper::complement(e2_->bases[i2]))
+		{
+			++mm_count;
+			int q1 = e1_->quality(i, params_.qoff);
+			int q2 = e2_->quality(i2, params_.qoff);
+
+			//debug output
+			if (params_.debug)
+			{
+				if (mm_count!=0)
+				{
+					debug_out << "R1: " << e1_->bases << endl;
+					debug_out << "Q1: "<< e1_->qualities << endl;
+					debug_out << "R2: "<< e2_->bases << endl;
+					debug_out << "Q2: "<< e2_->qualities << endl;
+				}
+				debug_out << "  MISMATCH index=" << i << " R1=" << e1_->bases[i] << "/" << q1 << " R2=" << e2_->bases[i2]<< "/" << q2 << endl;
+			}
+
+			//correct error
+			if (q1>q2)
+			{
+				char replacement = NGSHelper::complement(e1_->bases[i]);
+				if (params_.debug)
+				{
+					debug_out << "    CORRECTED R2: " << e2_->bases[i2] << " => " << replacement << endl;
+				}
+				e2_->bases[i2] = replacement;
+				++ecstats_.mismatch_r2[i2];
+			}
+			else if(q1<q2)
+			{
+				char replacement = NGSHelper::complement(e2_->bases[i2]);
+				if (params_.debug)
+				{
+					debug_out << "    CORRECTED R1: " << e1_->bases[i] << " => " << replacement << endl;
+				}
+				e1_->bases[i] = replacement;
+				++ecstats_.mismatch_r1[i];
+			}
+		}
+	}
+
+	if (mm_count>0)
+	{
+		++ecstats_.errors_per_read[mm_count];
+	}
+}
+
 void AnalysisWorker::run()
 {	
-	QTextStream debug_out (stdout);
+	QTextStream debug_out(stdout);
 
 	//check that headers match
 	QByteArray h1 = e1_->header.split(' ').at(0);
@@ -112,9 +171,9 @@ void AnalysisWorker::run()
 	int max_length = std::max(length_s1_orig, length_s2_orig);
 	
 	//check length
-	if (max_length>=stats_.bases_remaining.capacity())
+	if (max_length>=MAXLEN)
 	{
-		THROW(ProgrammingException, "Read length unsupported! A maximum read length of " + QString::number(stats_.bases_remaining.capacity()) + " is supported!");
+		THROW(ProgrammingException, "Read length unsupported! A maximum read length of " + QString::number(MAXLEN) + " is supported!");
 	}
 
 	//update QC statistics (has to be done before trimming)
@@ -279,6 +338,9 @@ void AnalysisWorker::run()
 		{
 			debug_out << "###Insert sequence hit - offset=" << best_offset << " prob=" << best_p << " adapter1=" << adapter1 << " adapter2=" << adapter2 << endl;
 		}
+
+		//error correction
+		if (params_.ec) correctErrors(debug_out);
 	}
 
 	//step 2: trim by adapter match - forward read
@@ -316,14 +378,6 @@ void AnalysisWorker::run()
 			double p = matchProbability(matches, mismatches);
 			if (p>params_.mep) continue;
 
-			//update consensus adapter sequence
-			QByteArray adapter = e1_->bases.right(length_s1_orig-offset);
-			if (adapter.count()>40) adapter.resize(40);
-			for (int i=0; i<adapter.count(); ++i)
-			{
-				stats_.acons1[i].inc(adapter.at(i));
-			}
-
 			//trim read
 			e1_->bases.resize(offset);
 			e1_->qualities.resize(offset);
@@ -331,6 +385,8 @@ void AnalysisWorker::run()
 
 			if (params_.debug)
 			{
+				QByteArray adapter = e1_->bases.right(length_s1_orig-offset);
+				adapter.truncate(20);
 				debug_out << "###Adapter 1 hit - offset=" << offset << " prob=" << p << " matches=" << matches << " mismatches=" << mismatches << " invalid=" << invalid << " adapter=" << adapter << endl;
 			}
 			break;
@@ -372,14 +428,6 @@ void AnalysisWorker::run()
 			double p = matchProbability(matches, mismatches);
 			if (p>params_.mep) continue;
 
-			//update consensus adapter sequence
-			QByteArray adapter = e2_->bases.right(length_s2_orig-offset);
-			if (adapter.count()>40) adapter.resize(40);
-			for (int i=0; i<adapter.count(); ++i)
-			{
-				stats_.acons2[i].inc(adapter.at(i));
-			}
-
 			//trim read
 			e2_->bases.resize(offset);
 			e2_->qualities.resize(offset);
@@ -389,6 +437,8 @@ void AnalysisWorker::run()
 
 			if (params_.debug)
 			{
+				QByteArray adapter = e2_->bases.right(length_s2_orig-offset);
+				adapter.truncate(20);
 				debug_out << "###Adapter 2 hit - offset=" << offset << " prob=" << p << " matches=" << matches << " mismatches=" << mismatches << " invalid=" << invalid << " adapter=" << adapter << endl;
 			}
 			break;
@@ -411,6 +461,9 @@ void AnalysisWorker::run()
 				e2_->bases.resize(offset_forward);
 				e2_->qualities.resize(offset_forward);
 			}
+
+			//error correction
+			if (params_.ec) correctErrors(debug_out);
 		}
 	}
 
@@ -465,10 +518,6 @@ void AnalysisWorker::run()
 	stats_.reads_trimmed_n += reads_trimmed_n;
 	stats_.reads_trimmed_q += reads_trimmed_q;
 	stats_.reads_removed += reads_removed;
-	if (max_length>=stats_.bases_remaining.count())
-	{
-		stats_.bases_remaining.resize(max_length+1);
-	}
 	stats_.bases_remaining[e1_->bases.length()] += 1;
 	stats_.bases_remaining[e2_->bases.length()] += 1;
 	stats_.bases_perc_trim_sum += (double)(length_s1_orig - e1_->bases.count()) / length_s1_orig;

src/SeqPurge/main.cpp
@@ -41,8 +41,13 @@ public:
 		addOutfile("summary", "Write summary/progress to this file instead of STDOUT.", true, true);
 		addOutfile("qc", "If set, a read QC file in qcML format is created (just like ReadQC).", true, true);
 		addInt("prefetch", "Maximum number of reads that may be pre-fetched to speed up trimming", true, 1000);
+		addFlag("ec", "Enable error-correction of reads with insert match.");
 		addFlag("debug", "Enables debug output (use only with one thread).");
 		addFlag("progress", "Enables progress output.");
+
+		//changelog
+		changeLog(2016, 4,  6, "Added error correction (optional).");
+		changeLog(2016, 3, 16, "Version used in the SeqPurge paper: [TODO add link]");
 	}
 
 	int processingReadPairs() const
@@ -88,6 +93,7 @@ public:
 
 		params_.qc = getOutfile("qc");
 		data_.analysis_pool.setMaxThreadCount(getInt("threads"));
+		params_.ec = getFlag("ec");
 		params_.debug = getFlag("debug");
 
 		QSharedPointer<QFile> outfile = Helper::openFileForWriting(getOutfile("summary"), true);
@@ -132,7 +138,7 @@ public:
 				QSharedPointer<FastqEntry> e2(new FastqEntry());
 				in2.readEntry(*e2);
 
-				data_.analysis_pool.start(new AnalysisWorker(e1, e2, params_, stats_, data_));
+				data_.analysis_pool.start(new AnalysisWorker(e1, e2, params_, stats_, ecstats_, data_));
 			}
 
 			//check that forward and reverse read file are both at the end
@@ -151,7 +157,7 @@ public:
 		data_.closeOutStreams();
 		if (progress) out << Helper::dateTime() << " closed - processing now: " << processingReadPairs() << " total processed: " << data_.reads_queued << endl;
 
-		//print statistics
+		//print trimming statistics
 		if (progress) out << Helper::dateTime() << " writing statistics summary" << endl;
 		stats_.writeStatistics(out, params_);
 
@@ -160,11 +166,19 @@ public:
 		{
 			stats_.qc.getResult().storeToQCML(getOutfile("qc"), QStringList() << in1_files << in2_files, "");
 		}
+
+		//print error correction statistics
+		if (params_.ec)
+		{
+			if (progress) out << Helper::dateTime() << " writing error corrections summary" << endl;
+			ecstats_.writeStatistics(out);
+		}
 	}
 
 private:
 	TrimmingParameters params_;
 	TrimmingStatistics stats_;
+	ErrorCorrectionStatistics ecstats_;
 	TrimmingData data_;
 };
 

src/SeqPurge/AnalysisWorker.h
@@ -10,7 +10,7 @@ class AnalysisWorker
         : public QRunnable
 {
 public:
-	AnalysisWorker(QSharedPointer<FastqEntry> e1, QSharedPointer<FastqEntry> e2, TrimmingParameters& params, TrimmingStatistics& stats, TrimmingData& data);
+	AnalysisWorker(QSharedPointer<FastqEntry> e1, QSharedPointer<FastqEntry> e2, TrimmingParameters& params, TrimmingStatistics& stats, ErrorCorrectionStatistics& ecstats, TrimmingData& data);
 	~AnalysisWorker();
 	void run();
 
@@ -22,6 +22,7 @@ private:
 	QSharedPointer<FastqEntry> e2_;
 	TrimmingParameters& params_;
 	TrimmingStatistics& stats_;
+	ErrorCorrectionStatistics& ecstats_;
 	TrimmingData& data_;
 
 	///Factorial calulation
@@ -33,6 +34,9 @@ private:
 
 	///Helper function for writing output in threads
 	void writeWithThread(FastqOutfileStream* stream, FastqEntry& entry);
+
+	///Error correction
+	void correctErrors(QTextStream& debug_out);
 };
 
 #endif

src/SeqPurge/Auxilary.h
@@ -6,6 +6,9 @@
 #include <Pileup.h>
 #include "StatisticsReads.h"
 
+
+const int MAXLEN = 1000;
+
 ///Output stream datastructure
 struct TrimmingData
 {
@@ -63,6 +66,7 @@ struct TrimmingParameters
 	int qwin;
 	int qoff;
 	int ncut;
+	bool ec;
 	bool debug;
 	QString qc;
 };
@@ -73,9 +77,9 @@ struct TrimmingStatistics
 	TrimmingStatistics()
 	: mutex()
 	, read_num(0)
-	, bases_remaining()
-	, acons1()
-	, acons2()
+	, bases_remaining(MAXLEN, 0) //fixed size - reallocation prevents parallel access
+	, acons1(40)
+	, acons2(40)
 	, reads_trimmed_insert(0.0)
 	, reads_trimmed_adapter(0.0)
 	, reads_trimmed_q(0.0)
@@ -84,13 +88,9 @@ struct TrimmingStatistics
 	, bases_perc_trim_sum(0.0)
 	, qc()
 	{
-		//reserve/resize array - reallocation of the array content can cause a crash because the array is accessed in parallel by several threads
-		bases_remaining.reserve(1000);
-		acons1.resize(40);
-		acons2.resize(40);
 	}
 
-	QMutex mutex; //Mutex for thread-safe writing
+	QMutex mutex; //Mutex for thread-safe access
 	int read_num;
 	QVector<double> bases_remaining;
 	QVector<Pileup> acons1;
@@ -152,13 +152,62 @@ struct TrimmingStatistics
 
 		//print length distribution after trimming
 		out << "Read length distribution after trimming:" << endl;
-		for (int i=0; i<bases_remaining.count(); ++i)
+		int max = bases_remaining.count()-1;
+		while(bases_remaining[max]==0) max -= 1;
+		for (int i=0; i<=max; ++i)
 		{
 			out << QString::number(i).rightJustified(4, ' ') << ": " << (long)(bases_remaining[i]) << endl;
 		}
 	}
 };
+///Statistics datastructure.
+struct ErrorCorrectionStatistics
+{
+	ErrorCorrectionStatistics()
+		: mismatch_r1(MAXLEN, 0) //fixed size - reallocation prevents parallel access
+		, mismatch_r2(MAXLEN, 0) //fixed size - reallocation prevents parallel access
+		, errors_per_read(MAXLEN, 0) //fixed size - reallocation prevents parallel access
+	{
+	}
 
+	QVector<long> mismatch_r1;
+	QVector<long> mismatch_r2;
+	QVector<long> errors_per_read;
+
+	void writeStatistics(QTextStream& out)
+	{
+		//print read error per cycle (read 1)
+		out << endl;
+		out << "Read error per cycle (read 1):" << endl;
+		int max = mismatch_r1.count()-1;
+		while(mismatch_r1[max]==0) max -= 1;
+		for (int i=1; i<=max; ++i)
+		{
+			out << QString::number(i).rightJustified(4, ' ') << ": " << mismatch_r1[i] << endl;
+		}
+
+		//print read error per cycle (read 2)
+		out << endl;
+		out << "Read error per cycle (read 2):" << endl;
+		max = mismatch_r2.count()-1;
+		while(mismatch_r2[max]==0) max -= 1;
+		for (int i=1; i<=max; ++i)
+		{
+			out << QString::number(i).rightJustified(4, ' ') << ": " << mismatch_r2[i] << endl;
+		}
+
+		//print error count distribution
+		out << endl;
+		out << "Read error count distribution:" << endl;
+		max = errors_per_read.count()-1;
+		while(errors_per_read[max]==0) max -= 1;
+		for (int i=1; i<=max; ++i)
+		{
+			out << QString::number(i).rightJustified(4, ' ') << ": " << errors_per_read[i] << endl;
+		}
+	}
+
+};
 
 #endif // AUXILARY_H
 

src/cppNGS/NGSHelper.cpp
@@ -478,7 +478,6 @@ QString NGSHelper::Cigar2QString(std::vector<CigarOp> Cigar)
 	return cigar_string;
 }
 
-///soft clip reads, nb: positions are 1-based
 void NGSHelper::softClipAlignment(BamAlignment& al, int start_ref_pos, int end_ref_pos)
 {
 	std::vector<CigarOp> old_CIGAR = al.CigarData;
@@ -642,10 +641,29 @@ QByteArray NGSHelper::changeSeq(const QByteArray& seq, bool rev, bool comp)
 					output[i] = 'N';
 					break;
 				default:
-					THROW(ProgrammingException, "Could not convert base " + QString(seq.at(i)) + " to reverse complement!");
+					THROW(ProgrammingException, "Could not convert base " + QString(seq.at(i)) + " to complement!");
 			}
 		}
 	}
 
 	return output;
+}
+
+char NGSHelper::complement(char base)
+{
+	switch(base)
+	{
+		case 'A':
+			return 'T';
+		case 'C':
+			return 'G';
+		case 'T':
+			return 'A';
+		case 'G':
+			return 'C';
+		case 'N':
+			return 'N';
+		default:
+			THROW(ProgrammingException, "Could not convert base " + QString(base) + " to complement!");
+	}
 }

src/cppNGS/NGSHelper.h
@@ -67,10 +67,12 @@ public:
 	*/
 	static void extractIndelsByCIGAR(QVector<Sequence>& indels, BamTools::BamAlignment& al, int pos, int indel_window=0);
 
+	///Returns the complement of a base
+	static char complement(char base);
 	///Changes a DNA sequence to reverse/complementary order.
 	static QByteArray changeSeq(const QByteArray& seq, bool rev, bool comp);
 
-	///Soft-clip alignment from the beginning or end
+	///Soft-clip alignment from the beginning or end (positions are 1-based)
 	static void softClipAlignment(BamTools::BamAlignment& al, int start_ref_pos, int end_ref_pos);
 
 	///Convert Cigar data to QString

commit b211c552be866125c1c90145c7f66e406d29db99

SeqPurge: fixed crash introduced in commit de762b55f701d9cc46f47b0ac2b394a2b01c5c4b

src/SeqPurge/AnalysisWorker.cpp
@@ -380,17 +380,19 @@ void AnalysisWorker::run()
 			double p = matchProbability(matches, mismatches);
 			if (p>params_.mep) continue;
 
-			//trim read
-			e1_->bases.resize(offset);
-			e1_->qualities.resize(offset);
-			offset_forward = offset;
-
+			//debug output
 			if (params_.debug)
 			{
 				QByteArray adapter = e1_->bases.right(length_s1_orig-offset);
 				adapter.truncate(20);
 				debug_out << "###Adapter 1 hit - offset=" << offset << " prob=" << p << " matches=" << matches << " mismatches=" << mismatches << " invalid=" << invalid << " adapter=" << adapter << endl;
 			}
+
+			//trim read
+			e1_->bases.resize(offset);
+			e1_->qualities.resize(offset);
+			offset_forward = offset;
+
 			break;
 		}
 
@@ -430,6 +432,14 @@ void AnalysisWorker::run()
 			double p = matchProbability(matches, mismatches);
 			if (p>params_.mep) continue;
 
+			//debug output
+			if (params_.debug)
+			{
+				QByteArray adapter = e2_->bases.right(length_s2_orig-offset);
+				adapter.truncate(20);
+				debug_out << "###Adapter 2 hit - offset=" << offset << " prob=" << p << " matches=" << matches << " mismatches=" << mismatches << " invalid=" << invalid << " adapter=" << adapter << endl;
+			}
+
 			//trim read
 			e2_->bases.resize(offset);
 			e2_->qualities.resize(offset);
@@ -437,12 +447,6 @@ void AnalysisWorker::run()
 			//update statistics
 			offset_reverse = offset;
 
-			if (params_.debug)
-			{
-				QByteArray adapter = e2_->bases.right(length_s2_orig-offset);
-				adapter.truncate(20);
-				debug_out << "###Adapter 2 hit - offset=" << offset << " prob=" << p << " matches=" << matches << " mismatches=" << mismatches << " invalid=" << invalid << " adapter=" << adapter << endl;
-			}
 			break;
 		}
 
@@ -463,9 +467,6 @@ void AnalysisWorker::run()
 				e2_->bases.resize(offset_forward);
 				e2_->qualities.resize(offset_forward);
 			}
-
-			//error correction
-			if (params_.ec) correctErrors(debug_out);
 		}
 	}
 

src/SeqPurge/main.cpp
@@ -41,7 +41,7 @@ public:
 		addOutfile("summary", "Write summary/progress to this file instead of STDOUT.", true, true);
 		addOutfile("qc", "If set, a read QC file in qcML format is created (just like ReadQC).", true, true);
 		addInt("prefetch", "Maximum number of reads that may be pre-fetched to speed up trimming", true, 1000);
-		addFlag("ec", "Enable error-correction of adapter-trimmed reads.");
+		addFlag("ec", "Enable error-correction of adapter-trimmed reads (only those with insert match).");
 		addFlag("debug", "Enables debug output (use only with one thread).");
 		addFlag("progress", "Enables progress output.");
 