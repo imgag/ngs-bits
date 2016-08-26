e6565f2bc16bea2c7068aa0bbc6590fcb251beeb
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2016-08-26 07:31:48 +0200

    FastqTrim: added 'len' parameter

--- src/FastqTrim/main.cpp
+++ src/FastqTrim/main.cpp
@@ -18,15 +18,19 @@ public:
 		setDescription("Trims start/end bases from all reads in a FASTQ file.");
 		addInfile("in", "Input gzipped FASTQ file.", false);
 		addOutfile("out", "Output gzipped FASTQ file.", false);
-		addInt("start", "Number of bases to trim from start of the read.", true, 0);
-		addInt("end", "Number of bases to trim from the end of the read.", true, 0);
+		addInt("start", "Trim this number of bases from the start of the read.", true, 0);
+		addInt("end", "Trim this number of bases from the end of the read.", true, 0);
+		addInt("len", "Restrict read length to this value (after trimming from start/end).", true, 0);
+
+		changeLog(2016, 8, 26, "Added 'len' parameter.");
 	}
 
 	virtual void main()
 	{
 		//init
-		int start = getInt("start");
-		int end = getInt("end");
+		const int start = getInt("start");
+		const int end = getInt("end");
+		const int len = getInt("len");
 
 		//process
 		FastqOutfileStream outstream(getOutfile("out"), false);
@@ -36,13 +40,21 @@ public:
 		{
 			stream.readEntry(entry);
 
-			int len = entry.bases.count();
-			if (len>start+end)
+			if (start>0 || end>0)
 			{
-				entry.bases = entry.bases.mid(start, len-start-end);
-				entry.qualities = entry.qualities.mid(start, len-start-end);
-				outstream.write(entry);
+				const int len2 = entry.bases.count();
+				if (len2<=start+end) continue;
+				entry.bases = entry.bases.mid(start, len2-start-end);
+				entry.qualities = entry.qualities.mid(start, len2-start-end);
 			}
+
+			if (len>0 && entry.bases.count()>len)
+			{
+				entry.bases.resize(len);
+				entry.qualities.resize(len);
+			}
+
+			outstream.write(entry);
 		}
 	}
 };
--- src/tools-TEST/FastqTrim_Test.h
+++ src/tools-TEST/FastqTrim_Test.h
@@ -5,16 +5,22 @@ TEST_CLASS(FastqTrim_Test)
 Q_OBJECT
 private slots:
 	
-	void test_01()
+	void start()
 	{
 		EXECUTE("FastqTrim", "-in " + TESTDATA("data_in/FastqTrim_in1.fastq.gz") + " -out out/FastqTrim_out1.fastq.gz -start 5");
 		COMPARE_GZ_FILES("out/FastqTrim_out1.fastq.gz", TESTDATA("data_out/FastqTrim_out1.fastq.gz"));
 	}
 	
-	void test_02()
+	void start_end()
 	{
 		EXECUTE("FastqTrim", "-in " + TESTDATA("data_in/FastqTrim_in1.fastq.gz") + " -out out/FastqTrim_out2.fastq.gz -start 5 -end 5");
 		COMPARE_GZ_FILES("out/FastqTrim_out2.fastq.gz", TESTDATA("data_out/FastqTrim_out2.fastq.gz"));
+	}
+
+	void start_len()
+	{
+		EXECUTE("FastqTrim", "-in " + TESTDATA("data_in/FastqTrim_in1.fastq.gz") + " -out out/FastqTrim_out3.fastq.gz -start 5 -len 50");
+		COMPARE_GZ_FILES("out/FastqTrim_out3.fastq.gz", TESTDATA("data_out/FastqTrim_out3.fastq.gz"));
 	}
 
 };
