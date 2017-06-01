0ee82f392f21b8ad5c4bc742f0f0eb4970a3503e
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2017-06-01 13:18:54 +0200

    GSvar: Added ExAC sup-population allele frequencies to details widget.

--- src/GSvar/VariantDetailsDockWidget.cpp
+++ src/GSvar/VariantDetailsDockWidget.cpp
@@ -104,6 +104,7 @@ void VariantDetailsDockWidget::updateVariant(const VariantList& vl, int index)
 	setAnnotation(ui->tg, vl, index, "1000g");
 	setAnnotation(ui->exac, vl, index, "ExAC");
 	setAnnotation(ui->exac_hom, vl, index, "ExAC_hom");
+	setAnnotation(ui->exac_sub, vl, index, "ExAC_sub");
 	setAnnotation(ui->kaviar, vl, index, "Kaviar");
 
 	//pathogenity predictions
