d8c5e5c4c944755051c454db6a01facfea1e6439
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2017-08-01 17:06:03 +0200

    test

--- tools/TravisCI/build.sh
+++ tools/TravisCI/build.sh
@@ -1,4 +1,1 @@
-grep -v '^#' /etc/apt/sources.list
-
-#addons: qt5-default libqt5xmlpatterns5-dev libqt5sql5-mysql git cmake python python-matplotlib
-#make build_3rdparty build_tools_release test_lib test_tools
+apt-get -y install qt5-default libqt5xmlpatterns5-dev libqt5sql5-mysql git cmake python python-matplotlib
