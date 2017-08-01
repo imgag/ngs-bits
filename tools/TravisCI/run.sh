0e7a25081b8c82e19cf29190f3c5d2344333a8ea
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2017-08-01 18:49:40 +0200

    test

--- tools/TravisCI/run.sh
+++ tools/TravisCI/run.sh
@@ -1,5 +1,4 @@
 apt-get update
 apt-get -y install g++ git cmake python python-matplotlib qt5-default libqt5xmlpatterns5-dev libqt5sql5-mysql
-make build_3rdparty build_tools_release 
-make test_lib 
-make test_tools
+make build_3rdparty build_libs_release build_tools_release
+make -k test_lib test_tools
