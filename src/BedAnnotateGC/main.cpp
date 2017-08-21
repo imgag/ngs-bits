29d732546ebdcc3c1495a76e00dcff05ea72b657
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2017-08-21 10:32:32 +0200

    BedAnnotateGC: added -extend and -clear parameters

--- src/BedAnnotateGC/main.cpp
+++ src/BedAnnotateGC/main.cpp
@@ -23,10 +23,16 @@ public:
 		addInfile("in", "Input BED file. If unset, reads from STDIN.", true);
 		addOutfile("out", "Output BED file. If unset, writes to STDOUT.", true);
 		addInfile("ref", "Reference genome FASTA file. If unset, 'reference_genome' from the 'settings.ini' file is used.", true, false);
+		addInt("extend", "Bases to extend around the input region for calculating the GC content.", true, 0);
+		addFlag("clear", "Clear all annotations present in the input file.");
 	}
 
 	virtual void main()
 	{
+		//init
+		int extend = getInt("extend");
+		bool clear = getFlag("clear");
+
 		//open refererence genome file
 		QString ref_file = getInfile("ref");
 		if (ref_file=="") ref_file = Settings::string("reference_genome");
@@ -35,13 +41,16 @@ public:
 		//load input
 		BedFile file;
 		file.load(getInfile("in"));
-		file.clearAnnotations();
+		if (clear)
+		{
+			file.clearAnnotations();
+		}
 
 		//annotate
 		for (int i=0; i<file.count(); ++i)
 		{
 			BedLine& r = file[i];
-			Sequence seq = reference.seq(r.chr(), r.start(), r.length(), true);
+			Sequence seq = reference.seq(r.chr(), r.start()-extend, r.length()+2*extend, true);
 			int gc = 0;
 			int at = 0;
 			for(int j=0; j<seq.length(); ++j)
--- src/tools-TEST/BedAnnotateGC_Test.h
+++ src/tools-TEST/BedAnnotateGC_Test.h
@@ -6,13 +6,22 @@ TEST_CLASS(BedAnnotateGC_Test)
 Q_OBJECT
 private slots:
 
-	void test_01()
+	void default_params()
 	{
 		QString ref_file = Settings::string("reference_genome");
 		if (ref_file=="") SKIP("Test needs the reference genome!");
 	
 		EXECUTE("BedAnnotateGC", "-in " + TESTDATA("data_in/BedAnnotateGC_in1.bed") + " -out out/BedAnnotateGC_out1.bed -ref " + ref_file);
 		COMPARE_FILES("out/BedAnnotateGC_out1.bed", TESTDATA("data_out/BedAnnotateGC_out1.bed"));
+	}
+
+	void clear_extend20()
+	{
+		QString ref_file = Settings::string("reference_genome");
+		if (ref_file=="") SKIP("Test needs the reference genome!");
+
+		EXECUTE("BedAnnotateGC", "-clear -extend 20 -in " + TESTDATA("data_in/BedAnnotateGC_in2.bed") + " -out out/BedAnnotateGC_out2.bed -ref " + ref_file);
+		COMPARE_FILES("out/BedAnnotateGC_out2.bed", TESTDATA("data_out/BedAnnotateGC_out2.bed"));
 	}
 
 };
