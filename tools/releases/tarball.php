b835782a955a9e5d35a68d45e4fd6a7b89f199a3
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-04-04 12:27:12 +0200

    Added script to create release tarball

---
+++ tools/releases/tarball.php
@@ -0,0 +1,51 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+include("/mnt/users/all/megSAP/src/Common/all.php");
+
+// parse command line arguments
+$parser = new ToolBase("tarball", "Creates a tarball for a tagged version of ngs-bits.");
+$parser->addString("tag", "Git tag.", false);
+extract($parser->parse($argv));
+
+//check out release version
+exec2("git clone --recursive https://github.com/imgag/ngs-bits.git");
+exec2("cd ngs-bits && git checkout {$tag}");
+exec2("cd ngs-bits && git submodule update --recursive --init");
+
+//remove test data
+exec2("find ngs-bits/ -type d -name 'data_*' | xargs rm -rf");
+
+//remove git infos
+exec2("find ngs-bits/ -name '.git*' | xargs rm -rf");
+
+//set fixed version
+$filename = "ngs-bits/src/cppCORE/cppCORE.pro";
+$file = file($filename);
+for($i=0; $i<count($file); ++$i)
+{
+	$line = $file[$i];
+	
+	//skip line that determines the version
+	if (starts_with($line, "SVN_VER="))
+	{
+		$file[$i] = "";
+	}
+	
+	//set fixed version
+	if (contains($line, '$$SVN_VER'))
+	{
+		$file[$i] = "DEFINES += \"CPPCORE_VERSION={$tag}\"\n";
+	}
+}
+file_put_contents($filename, $file);
+
+//create tarball
+$tarball = "ngs-bits-{$tag}.tgz";
+exec2("tar czf {$tarball} ngs-bits/");
+
+//calculate hash
+list($stdout) = exec2("shasum -a 256 {$tarball}");
+print "HASH:\n".implode("", $stdout)."\n";
+?>
