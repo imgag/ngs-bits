e65bf5398e7558f254565c3f96edd3be087e92aa
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-06-08 16:46:33 +0200

    UpdaHunter: added more test data

--- src/tools-TEST/UpdHunter_Test.h
+++ src/tools-TEST/UpdHunter_Test.h
@@ -12,4 +12,16 @@ private slots:
 		EXECUTE("UpdHunter", "-in " + TESTDATA("data_in/UpdHunter_in1.vcf.gz") + " -c CHILD -f FATHER -m MOTHER -exclude " + TESTDATA("data_in/UpdHunter_in1.bed") + " -out out/UpdHunter_out1.tsv");
         COMPARE_FILES("out/UpdHunter_out1.tsv", TESTDATA("data_out/UpdHunter_out1.tsv"));
 	}
+
+	void positive1()
+	{
+		EXECUTE("UpdHunter", "-in " + TESTDATA("data_in/UpdHunter_in2.vcf.gz") + " -c CHILD -f FATHER -m MOTHER -out out/UpdHunter_out2.tsv");
+		COMPARE_FILES("out/UpdHunter_out2.tsv", TESTDATA("data_out/UpdHunter_out2.tsv"));
+	}
+
+	void positive2()
+	{
+		EXECUTE("UpdHunter", "-in " + TESTDATA("data_in/UpdHunter_in3.vcf.gz") + " -c CHILD -f FATHER -m MOTHER -out out/UpdHunter_out3.tsv");
+		COMPARE_FILES("out/UpdHunter_out3.tsv", TESTDATA("data_out/UpdHunter_out3.tsv"));
+	}
 };
