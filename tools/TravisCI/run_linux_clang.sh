6eb71451bc0c14751825d26185d504d1bc5582a9
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-09-14 13:45:13 +0200

    [travis] Use docker containers for linux - use matrix builds again

---
+++ tools/TravisCI/run_linux_clang.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get -y install make clang git python python-matplotlib qt5-default libqt5xmlpatterns5-dev libqt5sql5-mysql libbz2-dev liblzma-dev
+
+make build_3rdparty build_libs_release build_tools_release build_gui_release
+make -k test_lib test_tools
