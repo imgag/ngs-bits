0a4c86b9bd0217ed35abf5d50fe4b82391375ed9
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-09-14 14:07:13 +0200

    [travis] Set clang as default c and c++ compiler on ubuntu

--- tools/TravisCI/run_linux_clang.sh
+++ tools/TravisCI/run_linux_clang.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt-get -y install make clang git python python-matplotlib qt5-default libqt5xmlpatterns5-dev libqt5sql5-mysql libbz2-dev liblzma-dev
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-3.8 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-3.8 100
 
 make build_3rdparty build_libs_release build_tools_release build_gui_release
 make -k test_lib test_tools
