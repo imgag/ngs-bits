0000a957e9e3ad9b21da1912005a6f0a011776da
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-09-20 11:11:34 +0200

    [test] Add seperate test results for OSX for BamDownsample - also fix fixtures for SampleDiff

--- src/tools-TEST/BamDownsample_Test.h
+++ src/tools-TEST/BamDownsample_Test.h
@@ -11,9 +11,11 @@ private slots:
 		EXECUTE("BamDownsample", "-in " + TESTDATA("data_in/BamDownsample_in1.bam") + " -out out/BamDownsample_out1.bam -percentage 20 -test");
 		IS_TRUE(QFile::exists("out/BamDownsample_out1.bam"));
 
-		//The random number generator behaves differently under Linux/Windows => we need separate expected test results
+        //The random number generator behaves differently under Linux/OSX/Windows => we need separate expected test results
 		#ifdef _WIN32
 			COMPARE_FILES("out/BamDownsample_Test_line11.log", TESTDATA("data_out/BamDownsample_out1_Windows.txt"));
+        #elif __APPLE__
+            COMPARE_FILES("out/BamDownSample_Test_line11.log", TESTDATA("data_out/BamDownSample_out1_OSX.txt"));
 		#else
 			COMPARE_FILES("out/BamDownsample_Test_line11.log", TESTDATA("data_out/BamDownsample_out1_Linux.txt"));
 		#endif
