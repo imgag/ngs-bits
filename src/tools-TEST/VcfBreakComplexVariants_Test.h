e1084c0630e43af023337666298bbf8de5126f4a
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-12-10 14:40:10 +0100

    [tests] Check correct vcf

--- src/tools-TEST/VcfBreakComplexVariants_Test.h
+++ src/tools-TEST/VcfBreakComplexVariants_Test.h
@@ -13,7 +13,7 @@ private slots:
 		// Test cases from VT, see https://github.com/atks/vt/tree/master/test/decompose_blocksub
 		EXECUTE("VcfBreakComplexVariants", "-in " + TESTDATA("data_in/VcfBreakComplexVariants_in01.vcf") + " -out out/VcfBreakComplexVariants_out01.vcf");
 		COMPARE_FILES("out/VcfBreakComplexVariants_out01.vcf", TESTDATA("data_out/VcfBreakComplexVariants_out01.vcf"));
-		VCF_IS_VALID("out/VcfAnnotateFromBed_out01.vcf");
+		VCF_IS_VALID("out/VcfBreakComplexVariants_out01.vcf");
 	}
 };
 
