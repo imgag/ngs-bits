3332e48dbb33d01b6a342105ae8e8012804e50ac
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2018-12-10 14:27:59 +0100

    [vcfbreakcomplexvariants] Simplify tests

--- src/tools-TEST/VcfBreakComplexVariants_Test.h
+++ src/tools-TEST/VcfBreakComplexVariants_Test.h
@@ -8,25 +8,12 @@ TEST_CLASS(VcfBreakComplexVariants_TEST)
 {
 Q_OBJECT
 private slots:
-	void even_length()
+	void test_vt_cases()
 	{
+		// Test cases from VT, see https://github.com/atks/vt/tree/master/test/decompose_blocksub
 		EXECUTE("VcfBreakComplexVariants", "-in " + TESTDATA("data_in/VcfBreakComplexVariants_in01.vcf") + " -out out/VcfBreakComplexVariants_out01.vcf");
 		COMPARE_FILES("out/VcfBreakComplexVariants_out01.vcf", TESTDATA("data_out/VcfBreakComplexVariants_out01.vcf"));
 		VCF_IS_VALID("out/VcfAnnotateFromBed_out01.vcf");
-	}
-
-	void uneven_length()
-	{
-		EXECUTE("VcfBreakComplexVariants", "-in " + TESTDATA("data_in/VcfBreakComplexVariants_in02.vcf") + " -out out/VcfBreakComplexVariants_out02.vcf");
-		COMPARE_FILES("out/VcfBreakComplexVariants_out02.vcf", TESTDATA("data_out/VcfBreakComplexVariants_out02.vcf"));
-		VCF_IS_VALID("out/VcfAnnotateFromBed_out02.vcf");
-	}
-
-	void phased_even_length()
-	{
-		EXECUTE("VcfBreakComplexVariants", "-in " + TESTDATA("data_in/VcfBreakComplexVariants_in03.vcf") + " -out out/VcfBreakComplexVariants_out03.vcf");
-		COMPARE_FILES("out/VcfBreakComplexVariants_out03.vcf", TESTDATA("data_out/VcfBreakComplexVariants_out03.vcf"));
-		VCF_IS_VALID("out/VcfAnnotateFromBed_out03.vcf");
 	}
 };
 
