be47f80c9deaa2ea51814b37411d46c0dfa96bc5
leonschuetz <leon.schuetz@med.uni-tuebingen.de>
2020-03-12 13:02:48 +0100

    src/tools-TEST/data_in/NGSDAddStructuralVariants_init.sql: updated test file

--- src/tools-TEST/data_in/NGSDAddVariantsGermline_init.sql
+++ src/tools-TEST/data_in/NGSDAddVariantsGermline_init.sql
@@ -19,8 +19,10 @@ INSERT INTO `sample` (`id`, `name`, `sample_type`, `species_id`, `gender`, `qual
 
 INSERT INTO `processing_system` (`id`, `name_short`, `name_manufacturer`, `adapter1_p5`, `adapter2_p7`, `type`, `shotgun`, `umi_type`, `target_file`, `genome_id`) VALUES
 (96, 'hpDYTv3', 'HaloPlex DYT v3', 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC', 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT', 'Panel Haloplex', 0, 'n/a', '/some_path/hpDYT_v3_2014_12_12.bed', 1),
-(145, 'ssHAEv6', 'SureSelectXT Human All Exon V6', 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC', 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT', 'WES', 1, 'n/a', '/some_path/ssHAEv6_2019_07_19.bed', 1);
+(145, 'ssHAEv6', 'SureSelectXT Human All Exon V6', 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC', 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT', 'WES', 1, 'n/a', '/some_path/ssHAEv6_2019_07_19.bed', 1),
+(165, 'TruSeqPCRfree', 'TruSeq DNA PCR-Free', 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC', 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT', 'WGS', 1, 'n/a', '/some_path/WGS_hg19.bed', 1);
 
 INSERT INTO `processed_sample`(`id`, `sample_id`, `process_id`, `sequencing_run_id`, `lane`, `processing_system_id`, `project_id`) VALUES
 (3999, 1, 18, 1, '1', 96, 1),
-(4000, 1, 38, 1, '2', 145, 1);
+(4000, 1, 38, 1, '2', 145, 1),
+(4001, 1, 45, 1, '1,2,3,4', 165, 1);
