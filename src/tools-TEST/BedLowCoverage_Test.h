eefebf7a037a870ce842ed6e60add9993cde24c9
Tim Stohn <ahstoht1@SRV018.img.med.uni-tuebingen.de>
2020-05-28 17:16:44 +0200

    - 2 tests for BedLowCoverage with set parameter min_baseq (once for roi and wgs)

--- src/tools-TEST/BedLowCoverage_Test.h
+++ src/tools-TEST/BedLowCoverage_Test.h
@@ -17,4 +17,16 @@ private slots:
         COMPARE_FILES("out/BedLowCoverage_test02_out.bed", TESTDATA("data_out/BedLowCoverage_test02_out.bed"));
     }
 
+	void roi_min_base_quality_cov()
+	{
+		EXECUTE("BedLowCoverage", "-in " + TESTDATA("../cppNGS-TEST/data_in/panel.bed") + " -bam " + TESTDATA("../cppNGS-TEST/data_in/panel.bam") + " -out out/BedLowCoverage_test03_out.bed -cutoff 20 -min_mapq 20 -min_baseq 30");
+		COMPARE_FILES("out/BedLowCoverage_test03_out.bed", TESTDATA("data_out/BedLowCoverage_test03_out.bed"));
+	}
+
+	void wgs_min_base_quality_cov()
+	{
+		EXECUTE("BedLowCoverage", "-wgs -bam " + TESTDATA("../cppNGS-TEST/data_in/panel.bam") + " -out out/BedLowCoverage_test04_out.bed -cutoff 20 -min_mapq 20 -min_baseq 30");
+		COMPARE_FILES("out/BedLowCoverage_test04_out.bed", TESTDATA("data_out/BedLowCoverage_test04_out.bed"));
+	}
+
 };
