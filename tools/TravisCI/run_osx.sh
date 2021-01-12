34caf2b4bef22539d2a0f51c4942aafe32870f3b
Alexandr Chernov <kstchernov@gmail.com>
2021-01-12 18:03:37 +0100

    Replace Travis CI with GitHub Actions (#115)

--- tools/TravisCI/run_osx.sh
+++
@@ -1,7 +0,0 @@
-brew update
-brew install qt lzlib
-brew link qt --force
-sudo pip install matplotlib
-
-make build_3rdparty build_libs_release build_tools_release build_gui_release
-make -k test_lib test_tools
