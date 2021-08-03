43a163f4bda3ffdce4d6daa85ace034d4be35552
SNE4K3R <72506135+SNE4K3R@users.noreply.github.com>
2021-08-03 11:01:41 +0200

    created MacOS-specific test data for GenePrioritization_Test

--- src/tools-TEST/GenePrioritization_Test.h
+++ src/tools-TEST/GenePrioritization_Test.h
@@ -21,6 +21,15 @@ private slots:
                 TESTDATA("data_in/GenePrioritization_graph.tsv") + " -out out/GenePrioritization_out2.tsv" +
 				" -method random_walk");
         IS_TRUE(QFile::exists("out/GenePrioritization_out2.tsv"));
-        COMPARE_FILES("out/GenePrioritization_out2.tsv", TESTDATA("data_out/GenePrioritization_out2.tsv"));
+
+        // random number generator behaves differently under different operating systems
+        if(Helper::isMacOS())
+        {
+            COMPARE_FILES("out/GenePrioritization_out2.tsv", TESTDATA("data_out/GenePrioritization_out2_OSX.tsv"));
+        }
+        else
+        {
+            COMPARE_FILES("out/GenePrioritization_out2.tsv", TESTDATA("data_out/GenePrioritization_out2.tsv"));
+        }
     }
 };
