c339a7adbaa10b8dba9081e41c789c51a7debaac
Alexandr Chernov <kstchernov@gmail.com>
2021-09-09 14:02:37 +0200

    Changed docker image for building Windows version of GSvar

--- deployment/windows/copy_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Copy all mingw dependencies  to make the app portable"
-mkdir dependencies
-cp /usr/i686-w64-mingw32/sys-root/mingw/bin/*.dll dependencies/
-cp /usr/i686-w64-mingw32/sys-root/mingw/lib/*.a dependencies/
-cp -r /usr/i686-w64-mingw32/sys-root/mingw/lib/qt5/plugins/* dependencies/
