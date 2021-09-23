29798e9a1988fd7ad581145ff85cf00ce8e0e19d
Leon Schütz <leonschuetz@users.noreply.github.com>
2021-09-23 08:29:04 +0200

    Vcf annotate with prefetch (#175)
    * src/VcfAnnotateFromBed/*: create jobs on the fly and use separate thread for file writing
    
    * src/VcfAnnotateFromBed/*: load input VCF on the fly
    
    * src/VcfAnnotateFrom*/: added prefetch parameter and minor code refactoring

--- src/tools-TEST/VcfAnnotateFromVcf_Test.h
+++ src/tools-TEST/VcfAnnotateFromVcf_Test.h
@@ -21,21 +21,21 @@ private slots:
 		VCF_IS_VALID("out/VcfAnnotateFromVcf_out2.vcf");
 	}
 
-    //multi-thread test
-    void test_multithread()
-    {
-        for (int i=2; i<=8; ++i)
-        {
-            QString suffix = QString::number(i) + "threads";
+	//multi-thread test
+	void test_multithread()
+	{
+		for (int i=2; i<=8; ++i)
+		{
+			QString suffix = QString::number(i) + "threads";
 
-            EXECUTE("VcfAnnotateFromVcf", "-in " + TESTDATA("data_in/VcfAnnotateFromVcf_in1.vcf") + " -out out/VcfAnnotateFromVcf_out1_" + suffix +".vcf -config_file " + TESTDATA("data_in/VcfAnnotateFromVcf_config.tsv") + " -block_size 30 -threads " + QString::number(i));
-            COMPARE_FILES("out/VcfAnnotateFromVcf_out1_" + suffix +".vcf", TESTDATA("data_out/VcfAnnotateFromVcf_out1.vcf"));
-            VCF_IS_VALID("out/VcfAnnotateFromVcf_out1_" + suffix +".vcf");
+			EXECUTE("VcfAnnotateFromVcf", "-in " + TESTDATA("data_in/VcfAnnotateFromVcf_in1.vcf") + " -out out/VcfAnnotateFromVcf_out1_" + suffix +".vcf -config_file " + TESTDATA("data_in/VcfAnnotateFromVcf_config.tsv") + " -block_size 30 -threads " + QString::number(i));
+			COMPARE_FILES("out/VcfAnnotateFromVcf_out1_" + suffix +".vcf", TESTDATA("data_out/VcfAnnotateFromVcf_out1.vcf"));
+			VCF_IS_VALID("out/VcfAnnotateFromVcf_out1_" + suffix +".vcf");
 
-            EXECUTE("VcfAnnotateFromVcf", "-in " + TESTDATA("data_in/VcfAnnotateFromVcf_in1.vcf") + " -out out/VcfAnnotateFromVcf_out2_" + suffix +".vcf -annotation_file " + TESTDATA("data_in/VcfAnnotateFromVcf_an2_NGSD.vcf.gz") + " -info_ids COUNTS,GSC01=GROUP,HAF,CLAS,CLAS_COM,COM -id_column ID -id_prefix NGSD -block_size 30 -threads " + QString::number(i) );
-            COMPARE_FILES("out/VcfAnnotateFromVcf_out2_" + suffix +".vcf", TESTDATA("data_out/VcfAnnotateFromVcf_out2.vcf"));
-            VCF_IS_VALID("out/VcfAnnotateFromVcf_out2_" + suffix +".vcf");
-        }
-    }
+			EXECUTE("VcfAnnotateFromVcf", "-in " + TESTDATA("data_in/VcfAnnotateFromVcf_in1.vcf") + " -out out/VcfAnnotateFromVcf_out2_" + suffix +".vcf -annotation_file " + TESTDATA("data_in/VcfAnnotateFromVcf_an2_NGSD.vcf.gz") + " -info_ids COUNTS,GSC01=GROUP,HAF,CLAS,CLAS_COM,COM -id_column ID -id_prefix NGSD -block_size 30 -threads " + QString::number(i) );
+			COMPARE_FILES("out/VcfAnnotateFromVcf_out2_" + suffix +".vcf", TESTDATA("data_out/VcfAnnotateFromVcf_out2.vcf"));
+			VCF_IS_VALID("out/VcfAnnotateFromVcf_out2_" + suffix +".vcf");
+		}
+	}
 
 };
