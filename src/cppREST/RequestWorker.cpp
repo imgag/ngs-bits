92581be3de727fe78f13e19b0647e866f01ba0bc
Alexandr Chernov <kstchernov@gmail.com>
2021-11-02 12:31:06 +0100

    Added socket wait timeout

--- src/cppREST/RequestWorker.cpp
+++ src/cppREST/RequestWorker.cpp
@@ -387,11 +387,11 @@ void RequestWorker::sendResponseDataPart(QSslSocket* socket, QByteArray data)
 	if (socket->state() != QSslSocket::SocketState::UnconnectedState)
 	{
 		socket->write(data);
-//		if (socket->bytesToWrite()) socket->waitForBytesWritten();
+		if (socket->bytesToWrite()) socket->waitForBytesWritten();
 	}
 
 	// clinet completes/cancels the stream or simply disconnects
-	if (socket->state() == QSslSocket::SocketState::UnconnectedState) closeAndDeleteSocket(socket);
+	if ((socket->state() == QSslSocket::SocketState::UnconnectedState) || (socket->state() == QSslSocket::SocketState::ClosingState)) closeAndDeleteSocket(socket);
 }
 
 void RequestWorker::sendEntireResponse(QSslSocket* socket, HttpResponse response)
