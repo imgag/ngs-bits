9b32f60e237ef0e5bae607557ae01f4bb1d69af4
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2021-12-07 15:05:43 +0100

    src/tools-TEST/CfDnaQC_Test.h: made test optional (requires reference genome)

--- src/tools-TEST/CfDnaQC_Test.h
+++ src/tools-TEST/CfDnaQC_Test.h
@@ -7,6 +7,9 @@ private slots:
 
 	void base_test1()
 	{
+		QString ref_file = Settings::string("reference_genome", true);
+		if (ref_file=="") SKIP("Test needs the reference genome!");
+
 		EXECUTE("CfDnaQC", "-bam " + TESTDATA("data_in/CfDnaQC_in_cfdna1.bam") + " -cfdna_panel " + TESTDATA("data_in/CfDnaQC_in_panel.bed") + " -out out/CfDnaQC_out1.qcML");
 		REMOVE_LINES("out/CfDnaQC_out1.qcML", QRegExp("creation "));
 		REMOVE_LINES("out/CfDnaQC_out1.qcML", QRegExp("<binary>"));
@@ -15,6 +18,9 @@ private slots:
 
 	void base_test2()
 	{
+		QString ref_file = Settings::string("reference_genome", true);
+		if (ref_file=="") SKIP("Test needs the reference genome!");
+
 		EXECUTE("CfDnaQC", "-bam " + TESTDATA("data_in/CfDnaQC_in_cfdna2.bam") + " -cfdna_panel " + TESTDATA("data_in/CfDnaQC_in_panel.bed") + " -out out/CfDnaQC_out2.qcML");
 		REMOVE_LINES("out/CfDnaQC_out2.qcML", QRegExp("creation "));
 		REMOVE_LINES("out/CfDnaQC_out2.qcML", QRegExp("<binary>"));
@@ -23,6 +29,9 @@ private slots:
 
 	void tumor_test()
 	{
+		QString ref_file = Settings::string("reference_genome", true);
+		if (ref_file=="") SKIP("Test needs the reference genome!");
+
 		EXECUTE("CfDnaQC", "-bam " + TESTDATA("data_in/CfDnaQC_in_cfdna1.bam") + " -tumor_bam " + TESTDATA("data_in/CfDnaQC_in_tumor.bam") + " -cfdna_panel "
 				+ TESTDATA("data_in/CfDnaQC_in_panel.bed") + " -out out/CfDnaQC_out3.qcML");
 		REMOVE_LINES("out/CfDnaQC_out3.qcML", QRegExp("creation "));
@@ -32,6 +41,9 @@ private slots:
 
 	void cfdna_relation_test()
 	{
+		QString ref_file = Settings::string("reference_genome", true);
+		if (ref_file=="") SKIP("Test needs the reference genome!");
+
 		EXECUTE("CfDnaQC", "-bam " + TESTDATA("data_in/CfDnaQC_in_cfdna1.bam") + " -related_bams " + TESTDATA("data_in/CfDnaQC_in_cfdna2.bam") + " -cfdna_panel "
 				+ TESTDATA("data_in/CfDnaQC_in_panel.bed") + " -out out/CfDnaQC_out4.qcML");
 		REMOVE_LINES("out/CfDnaQC_out4.qcML", QRegExp("creation "));
