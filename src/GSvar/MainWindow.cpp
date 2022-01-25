f793c83e452d9314e93ac3ddff6ed8fcc17ef530
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-01-25 08:12:12 +0100

    add debug prints

--- src/GSvar/MainWindow.cpp
+++ src/GSvar/MainWindow.cpp
@@ -129,6 +129,8 @@ QT_CHARTS_USE_NAMESPACE
 #include "CacheInitWorker.h"
 #include "BlatWidget.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, ui_()
@@ -6144,15 +6146,26 @@ void MainWindow::applyFilters(bool debug_time)
 			filter_phenos_ = false;
 			last_phenos_ = phenos;
 
+			qDebug() << "allowed Sources: " << last_phenotype_sources_.length() << "\n";
+			foreach (PhenotypeSource::Source s, last_phenotype_sources_)
+			{
+				qDebug() << PhenotypeSource::sourceToString(s) << "\n";
+			}
+
+			qDebug() << "allowed Evidences: " << last_phenotype_evidences_.length() << "\n";
+			foreach (PhenotypeEvidence::Evidence e, last_phenotype_evidences_)
+			{
+				qDebug() << PhenotypeEvidence::evidenceToString(e) << "\n";
+			}
+
 			//convert phenotypes to genes
 			NGSD db;
 			GeneSet pheno_genes;
 			foreach(const Phenotype& pheno, phenos)
 			{
-
 				pheno_genes << db.phenotypeToGenesbySourceAndEvidence(db.phenotypeIdByAccession(pheno.accession()), last_phenotype_sources_, last_phenotype_evidences_, true, false);
 			}
-
+			qDebug() << "Found genes: " << pheno_genes.count() << "\n";
 			//convert genes to ROI (using a cache to speed up repeating queries)
 			last_phenos_roi_.clear();
 			foreach(const QByteArray& gene, pheno_genes)
