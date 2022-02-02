4ffd89e06a59e7e83599db9b82638f9cc2ddf9df
Alexander Ott <30521779+Ott-Alexander@users.noreply.github.com>
2022-02-02 15:13:40 +0100

    VcfAnnotateFromBigWig: added mode flag (#195)

--- src/tools-TEST/VcfAnnotateFromBigWig_Test.h
+++ src/tools-TEST/VcfAnnotateFromBigWig_Test.h
@@ -11,22 +11,43 @@ private slots:
 
 	void test01()
 	{
-		EXECUTE("VcfAnnotateFromBigWig", "-in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in.vcf") + " -out out/VcfAnnotateFromBigWig_out1.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		EXECUTE("VcfAnnotateFromBigWig", "-mode max -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in.vcf") + " -out out/VcfAnnotateFromBigWig_out1.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
 		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out1.vcf");
 		COMPARE_FILES("out/VcfAnnotateFromBigWig_out1.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out1.vcf"))
 	}
 
 	void test01_multithreaded()
 	{
-		EXECUTE("VcfAnnotateFromBigWig", "-threads 5 -block_size 25 -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in.vcf") + " -out out/VcfAnnotateFromBigWig_out1.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		EXECUTE("VcfAnnotateFromBigWig", "-mode max -threads 5 -block_size 25 -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in.vcf") + " -out out/VcfAnnotateFromBigWig_out1.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
 		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out1.vcf");
 		COMPARE_FILES("out/VcfAnnotateFromBigWig_out1.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out1.vcf"))
 	}
 
-	void test02()
+	void mode_max()
 	{
-		EXECUTE("VcfAnnotateFromBigWig", "-in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in2.vcf") + " -out out/VcfAnnotateFromBigWig_out2.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		EXECUTE("VcfAnnotateFromBigWig", "-mode max -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in2.vcf") + " -out out/VcfAnnotateFromBigWig_out2.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
 		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out2.vcf");
 		COMPARE_FILES("out/VcfAnnotateFromBigWig_out2.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out2.vcf"))
+	}
+
+	void mode_min()
+	{
+		EXECUTE("VcfAnnotateFromBigWig", "-mode min -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in2.vcf") + " -out out/VcfAnnotateFromBigWig_out3.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out2.vcf");
+		COMPARE_FILES("out/VcfAnnotateFromBigWig_out3.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out3.vcf"))
+	}
+
+	void mode_avg()
+	{
+		EXECUTE("VcfAnnotateFromBigWig", "-mode avg -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in2.vcf") + " -out out/VcfAnnotateFromBigWig_out4.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out2.vcf");
+		COMPARE_FILES("out/VcfAnnotateFromBigWig_out4.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out4.vcf"))
+	}
+
+	void mode_none()
+	{
+		EXECUTE("VcfAnnotateFromBigWig", "-mode none -in " + TESTDATA("data_in/VcfAnnotateFromBigWig_in2.vcf") + " -out out/VcfAnnotateFromBigWig_out5.vcf -name PHYLOP -desc phylop_value -bw " + TESTDATA("data_in/VcfAnnotateFromBigWig_phyloP_chr1_part.bw"));
+		VCF_IS_VALID("out/VcfAnnotateFromBigWig_out2.vcf");
+		COMPARE_FILES("out/VcfAnnotateFromBigWig_out5.vcf", TESTDATA("data_out/VcfAnnotateFromBigWig_out5.vcf"))
 	}
 };
