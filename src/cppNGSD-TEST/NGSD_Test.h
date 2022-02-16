ee8527b36aa396d4a79ab3fcf09f37daa7678bca
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-02-16 09:52:26 +0100

    switch report xml to version 6: include SVs and ChromosomeAliases table

--- src/cppNGSD-TEST/NGSD_Test.h
+++ src/cppNGSD-TEST/NGSD_Test.h
@@ -1418,6 +1418,7 @@ private slots:
 			generator.writeXML("out/germline_report1.xml", "out/germline_report1.html");
 			COMPARE_FILES("out/germline_report1.xml", TESTDATA("data_out/germline_report1.xml"));
 		}
+		qDebug("Test 1 - pass");
 
 		//############################### TEST 2 - with variants, with target region, all optional parts enabled ###############################
 		{
@@ -1479,12 +1480,13 @@ private slots:
 			GermlineReportGenerator generator(data, true);
 			generator.overrideDate(report_date);
 			generator.writeHTML("out/germline_report2.html");
-			COMPARE_FILES("out/germline_report2.html", TESTDATA("data_out/germline_report2.html"));
 			generator.writeXML("out/germline_report2.xml", "out/germline_report2.html");
+			COMPARE_FILES("out/germline_report2.html", TESTDATA("data_out/germline_report2.html"));
+
 			COMPARE_FILES("out/germline_report2.xml", TESTDATA("data_out/germline_report2.xml"));
 		}
 
-
+		qDebug("Test 2 - pass");
 		//############################### TEST 3 - english ###############################
 		{
 			report_settings.language = "english";
@@ -1496,7 +1498,7 @@ private slots:
 			COMPARE_FILES("out/germline_report3.html", TESTDATA("data_out/germline_report3.html"));
 		}
 
-
+		qDebug("Test 3 - pass");
 		//############################### TEST 4 - evaluation sheet ###############################
 		{
 			GermlineReportGenerator generator(data, true);
@@ -1529,6 +1531,7 @@ private slots:
 			generator.writeEvaluationSheet("out/germline_sheet1.html", sheet_data);
 			COMPARE_FILES("out/germline_sheet1.html", TESTDATA("data_out/germline_sheet1.html"));
 		}
+		qDebug("Test 4 - pass");
 
 	}
 
