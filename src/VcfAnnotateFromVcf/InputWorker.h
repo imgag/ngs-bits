6e3b014f30c2dec19fa596d591b57db5a7dd1f39
Marc Sturm <marc-sturm@users.noreply.github.com>
2022-02-24 13:02:43 +0100

    VcfAnnotateFromVcf: refactoring and change to event-driven architecture (#212)

--- src/VcfAnnotateFromVcf/InputWorker.h
+++ src/VcfAnnotateFromVcf/InputWorker.h
@@ -1,27 +1,30 @@
-#ifndef CHUNKPROCESSOR_H
-#define CHUNKPROCESSOR_H
+#ifndef INPUTWORKER_H
+#define INPUTWORKER_H
 
 #include <QObject>
 #include <QRunnable>
+#include <zlib.h>
 #include "Auxilary.h"
 
-class ChunkProcessor
+class InputWorker
 	: public QObject
 	, public QRunnable
 {
 	Q_OBJECT
 
 public:
-	ChunkProcessor(AnalysisJob &job, const MetaData& meta);
+	InputWorker(AnalysisJob& job, gzFile& in_stream, Parameters& params);
 	virtual void run() override;
 
 signals:
 	void done(int i); //signal emitted when job was successful
 	void error(int i, QString message); //signal emitted when job failed
+	void inputDone(int i); //signal emitted when all input data was read
 
 private:
 	AnalysisJob& job_;
-	const MetaData& meta_;
+	gzFile& in_stream_;
+	Parameters& params_;
 };
 
-#endif // CHUNKPROCESSOR_H
+#endif // INPUTWORKER_H
