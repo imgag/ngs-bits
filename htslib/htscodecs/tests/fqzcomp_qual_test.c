207278c253620fb681eb8974aa05dc7e8d2b9950
Alexandr Chernov <kstchernov@gmail.com>
2022-04-22 13:22:42 +0200

    Updated htslib to 1.15.1 with libcurl option enabled

--- htslib/htscodecs/tests/fqzcomp_qual_test.c
+++ htslib/htscodecs/tests/fqzcomp_qual_test.c
@@ -1,6 +1,6 @@
 /* Tests for fqz codec */
 /*
- * Copyright (c) 2019,2020 Genome Research Ltd.
+ * Copyright (c) 2019-2021 Genome Research Ltd.
  * Author(s): James Bonfield
  *
  * Redistribution and use in source and binary forms, with or without
