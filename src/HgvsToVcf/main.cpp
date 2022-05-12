53bce65c596ba3413ec86583758701b6bac18d4a
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-05-12 13:02:49 +0200

    HgvsToVcf: add test case and hg19 warning

--- src/HgvsToVcf/main.cpp
+++ src/HgvsToVcf/main.cpp
@@ -168,6 +168,12 @@ public:
 		QSharedPointer<QFile> instream = Helper::openFileForReading(in, true);
 		QSharedPointer<QFile> outstream = Helper::openFileForWriting(out, false);
 
+		if (getEnum("build") == "hg19")
+		{
+			QTextStream out(stderr);
+			out << "Warning: When using the hg19 build, it is neccessary to also use a NGSD instance containing hg19 data and a hg19 reference genome.\n";
+		}
+
 		const QMap<QByteArray, QByteArrayList>& transcript_matches = NGSHelper::transcriptMatches(stringToBuild(getEnum("build")));
 
 		QStringList tsv_headers = {"", ""}; //fallback in case there is no header
