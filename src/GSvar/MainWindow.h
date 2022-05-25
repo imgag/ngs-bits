0a1f2366e1d6f2941d0e7781b1f26989a4736dd7
axelgschwind <axel.gschwind@t-online.de>
2022-05-25 14:50:58 +0200

    Added widget for adding somatic gene pathways

--- src/GSvar/MainWindow.h
+++ src/GSvar/MainWindow.h
@@ -167,6 +167,8 @@ public slots:
 	void on_actionPreferredTranscripts_triggered();
 	///Somatic gene roles
 	void on_actionEditSomaticGeneRoles_triggered();
+	///Somatic gene pathways
+	void on_actionEditSomaticPathways_triggered();
 	///Opens online documentation
 	void on_actionOpenDocumentation_triggered();
 	///Approved symbols dialog
