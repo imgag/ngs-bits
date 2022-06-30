5eb7d069dcd01e314222c0e0b962e1b48737c3ad
Marvin Döbel <AHDoebM1@ukt.ad.local>
2022-06-30 09:45:15 +0200

    update Hexplorer tests

--- src/tools-TEST/Hexplorer_Test.h
+++ src/tools-TEST/Hexplorer_Test.h
@@ -8,6 +8,9 @@ private slots:
 	
 	void default_parameters()
 	{
+        QString ref_file = Settings::string("reference_genome", true);
+        if (ref_file=="") SKIP("Test needs the reference genome!");
+
         EXECUTE("Hexplorer", "-in " + TESTDATA("data_in/hexplorer_variants.vcf") + " -out out/hexplorer_output.vcf");
         COMPARE_FILES("out/hexplorer_output.vcf", TESTDATA("data_out/hexplorer_output.vcf"));
         VCF_IS_VALID("out/hexplorer_output.vcf");
