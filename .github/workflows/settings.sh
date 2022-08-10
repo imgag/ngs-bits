9667a844197a93f2d9e215712e890f7afbcc759f
Alexandr Chernov <kstchernov@gmail.com>
2022-08-10 11:17:20 +0200

    GSvar: enabled database tests in Linux CI environment

--- .github/workflows/settings.sh
+++ .github/workflows/settings.sh
@@ -3,9 +3,9 @@ mkdir ./projects_folder
 touch ./bin/settings.ini
 echo "ngsd_test_host = \"127.0.0.1\"" >> ./bin/settings.ini
 echo "ngsd_test_port = 3306" >> ./bin/settings.ini
-echo "ngsd_test_name = \"root\"" >> ./bin/settings.ini
-echo "ngsd_test_user = \"root\"" >> ./bin/settings.ini
-echo "ngsd_test_pass = \"\"" >> ./bin/settings.ini
+echo "ngsd_test_name = \"testdb\"" >> ./bin/settings.ini
+echo "ngsd_test_user = \"testuser\"" >> ./bin/settings.ini
+echo "ngsd_test_pass = \"password\"" >> ./bin/settings.ini
 echo "projects_folder = \"projects_folder\"" >> ./bin/settings.ini
 echo "data_folder = \"\"" >> ./bin/settings.ini
 echo "url_lifetime = 5" >> ./bin/settings.ini
