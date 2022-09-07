9d725bf086dd74fcbf09e013ce7739920ac8fc15
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2022-09-07 10:40:56 +0200

    VcfToTsv: Added support for streaming

--- src/VcfToTsv/main.cpp
+++ src/VcfToTsv/main.cpp
@@ -22,10 +22,12 @@ public:
 		setDescription("Converts a VCF file to a tab-separated text file.");
         setExtendedDescription(QStringList() << "Multi-allelic variants are supported. All alternative sequences are stored as a comma-seperated list."
                                              << "Multi-sample VCFs are supported. For every combination of FORMAT and SAMPLE a seperate column is generated and named in the following way: <SAMPLEID>_<FORMATID>_<format>.");
-		addInfile("in", "Input variant list in VCF format.", false, true);
-		addOutfile("out", "Output variant list in TSV format.", false, true);
+		//optional
+		addInfile("in", "Input variant list in VCF format. If unset, reads from STDIN.", true, true);
+		addOutfile("out", "Output variant list in TSV format. If unset, writes to STDOUT.", true, true);
 
-        changeLog(2020,  8, 07, "Multi-allelic and Multi-sample VCFs are supported.");
+		changeLog(2022,  9,  7, "Added support for streaming (STDIN > STDOUT).");
+		changeLog(2020,  8,  7, "Multi-allelic and multi-sample VCFs are supported.");
 	}
 
 	virtual void main()
