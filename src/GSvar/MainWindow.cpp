652334387e73b888d9ef7ca12115ec6d7a7982e8
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2022-10-19 10:30:39 +0200

    MainWindow: load ontology terms for somatic report only once

--- src/GSvar/MainWindow.cpp
+++ src/GSvar/MainWindow.cpp
@@ -4470,13 +4470,16 @@ void MainWindow::generateReportSomaticRTF()
 	somatic_report_settings_.preferred_transcripts = GSvarHelper::preferredTranscripts();
 
 	//load obo terms for filtering coding/splicing variants
-	OntologyTermCollection obo_terms("://Resources/so-xp_3_0_0.obo",true);
-	QList<QByteArray> ids;
-	ids << obo_terms.childIDs("SO:0001580",true); //coding variants
-	ids << obo_terms.childIDs("SO:0001568",true); //splicing variants
-	foreach(const QByteArray& id, ids)
+	if (somatic_report_settings_.obo_terms_coding_splicing.size() == 0)
 	{
-		somatic_report_settings_.obo_terms_coding_splicing.add(obo_terms.getByID(id));
+		OntologyTermCollection obo_terms("://Resources/so-xp_3_0_0.obo",true);
+		QList<QByteArray> ids;
+		ids << obo_terms.childIDs("SO:0001580",true); //coding variants
+		ids << obo_terms.childIDs("SO:0001568",true); //splicing variants
+		foreach(const QByteArray& id, ids)
+		{
+			somatic_report_settings_.obo_terms_coding_splicing.add(obo_terms.getByID(id));
+		}
 	}
 
 	somatic_report_settings_.target_region_filter = ui_.filters->targetRegion();
