2a9ef1ac6e2630e5287eb24f40b897fb7f24883b
Jakob Admard <jakob.admard@med.uni-tuebingen.de>
2023-02-17 16:10:11 +0100

    NGSD schema: Added quantification method

--- src/cppNGSD/resources/NGSD_schema.sql
+++ src/cppNGSD/resources/NGSD_schema.sql
@@ -217,7 +217,7 @@ CREATE  TABLE IF NOT EXISTS `sequencing_run`
   `device_id` INT(11) NOT NULL,
   `recipe` VARCHAR(45) NOT NULL COMMENT 'Read length for reads and index reads separated by \'+\'',
   `pool_molarity` float DEFAULT NULL,
-  `pool_quantification_method` enum('n/a','Tapestation','Bioanalyzer','qPCR','Tapestation & Qubit','Bioanalyzer & Qubit','Bioanalyzer & Tecan Infinite','Fragment Analyzer & Qubit','Fragment Analyzer & Tecan Infinite','Illumina 450bp & Qubit ssDNA','PCR Size & ssDNA') NOT NULL DEFAULT 'n/a',
+  `pool_quantification_method` enum('n/a','Tapestation','Bioanalyzer','qPCR','Tapestation & Qubit','Bioanalyzer & Qubit','Bioanalyzer & Tecan Infinite','Fragment Analyzer & Qubit','Fragment Analyzer & Tecan Infinite','Illumina 450bp & Qubit ssDNA','PCR Size & ssDNA','FemtoPulse & Qubit') NOT NULL DEFAULT 'n/a',
   `comment` TEXT NULL DEFAULT NULL,
   `quality` ENUM('n/a','good','medium','bad') NOT NULL DEFAULT 'n/a',
   `status` ENUM('n/a','run_started','run_finished','run_aborted','demultiplexing_started','analysis_started','analysis_finished','analysis_not_possible','analysis_and_backup_not_required') NOT NULL DEFAULT 'n/a',
