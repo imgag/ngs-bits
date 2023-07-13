bbaaab162e1bc774af47acc6b47f2ef045283a23
MarvinDo <qarst@gmx.de>
2023-07-13 09:34:29 +0200

    added tests for vcfannotatemaxentscan

---
+++ src/tools-TEST/VcfAnnotateMaxEntScan_Test.h
@@ -0,0 +1,19 @@
+#include "TestFramework.h"
+#include "TestFrameworkNGS.h"
+
+TEST_CLASS(VcfAnnotateMaxEntScan_Test)
+{
+Q_OBJECT
+private slots:
+	
+	void default_parameters()
+	{
+        QString ref_file = Settings::string("reference_genome", true);
+        if (ref_file=="") SKIP("Test needs the reference genome!");
+
+		EXECUTE("VcfAnnotateMaxEntScan", "-in " + TESTDATA("data_in/VcfAnnotateMaxEntScan.vcf") + " -out out/VcfAnnotateMaxEntScan_out.vcf" + " -gff " + TESTDATA("data_in/VcfAnnotateMaxEntScan_transcripts.gff3") + " -mes" + " -swa");
+		COMPARE_FILES("out/VcfAnnotateMaxEntScan_out.vcf", TESTDATA("data_out/VcfAnnotateMaxEntScan_out.vcf"));
+		VCF_IS_VALID("out/VcfAnnotateMaxEntScan_out.vcf");
+	}
+
+};
