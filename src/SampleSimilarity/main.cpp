34838687945aa93267ef384f8a802e3823e3b1e8
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2023-12-22 16:24:15 +0100

    GSvar: using hg38 coding ROI for sample similarity calculations now to make scores comparable between different kits

--- src/SampleSimilarity/main.cpp
+++ src/SampleSimilarity/main.cpp
@@ -20,7 +20,7 @@ public:
 	virtual void setup()
 	{
 		setDescription("Calculates pairwise sample similarity metrics from VCF/BAM/CRAM files.");
-		setExtendedDescription(QStringList() << "In VCF mode, multi-allelic variants are not supported. Use 'skip_multi' to ignore them, or VcfBreakMulti to split multi-allelic variants into several lines."
+		setExtendedDescription(QStringList() << "In VCF mode, multi-allelic variants are not supported and skipped. Use VcfBreakMulti to split multi-allelic variants into several lines if you want to use them."
 											 << "Multi-sample VCFs are not supported. Use VcfExtractSamples to split them to one VCF per sample."
 											 << "In VCF mode, it is assumed that variant lists are left-normalized, e.g. with VcfLeftNormalize."
 											 << "BAM mode supports BAM as well as CRAM files."
@@ -33,7 +33,6 @@ public:
 		addInfile("roi", "Restrict similarity calculation to variants in target region.", true);
 		addFlag("roi_hg38_wes_wgs", "Used pre-defined high-confidence coding region of hg38. Speeds up calculations, especially for WGS. Also makes scores comparable when mixing WES and WGS or different WES kits.");
 		addFlag("include_gonosomes", "Includes gonosomes into calculation (by default only variants on autosomes are considered).");
-		addFlag("skip_multi", "Skip multi-allelic variants instead of throwing an error (VCF mode).");
 		addInt("min_cov",  "Minimum coverage to consider a SNP for the analysis (BAM mode).",  true,  30);
 		addInt("max_snps",  "The maximum number of high-coverage SNPs to extract from BAM/CRAM. 0 means unlimited (BAM mode).",  true, 5000);
 		addEnum("build", "Genome build used to generate the input (BAM mode).", true, QStringList() << "hg19" << "hg38", "hg38");
@@ -46,7 +45,6 @@ public:
 		changeLog(2022,  6, 30, "Changed GSvar mode: MODIFIER impact variants are now ingnored to make scores more similar between exomes and genomes.");
 		changeLog(2020, 11, 27, "Added CRAM support.");
 		changeLog(2019,  2,  8, "Massive speed-up by caching of variants/genotypes instead of loading them again for each comparison.");
-		changeLog(2018, 11, 26, "Add flag 'skip_multi' to ignore multi-allelic sites.");
 		changeLog(2018,  7, 11, "Added build switch for hg38 support.");
 		changeLog(2018,  6, 20, "Added IBS0 and IBS2 metrics and renamed tool to SampleSimilarity (was SampleCorrelation).");
 		changeLog(2018,  1,  5, "Added multi-sample support and VCF input file support.");
@@ -70,7 +68,6 @@ public:
 		bool roi_hg38_wes_wgs = getFlag("roi_hg38_wes_wgs");
 		bool include_gonosomes = getFlag("include_gonosomes");
 		GenomeBuild build = stringToBuild(getEnum("build"));
-		bool skip_multi = getFlag("skip_multi");
 		bool debug = getFlag("debug");
 		QTime timer;
 		timer.start();
@@ -102,7 +99,7 @@ public:
 		{
 			if (mode=="vcf")
 			{
-				genotype_data << (roi_reg.count()>0 ? SampleSimilarity::genotypesFromVcf(filename, include_gonosomes, skip_multi, roi_reg) : SampleSimilarity::genotypesFromVcf(filename, include_gonosomes, skip_multi));
+				genotype_data << (roi_reg.count()>0 ? SampleSimilarity::genotypesFromVcf(filename, include_gonosomes, true, roi_reg) : SampleSimilarity::genotypesFromVcf(filename, include_gonosomes, true));
 			}
 			else if(mode=="gsvar")
 			{
