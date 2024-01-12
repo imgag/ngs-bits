imgag/ngs-bits

commit 932bd4f896ede0b6ec8fa13e842d406cdbcfe862

GSvar: best transcript is automatically shown if there is no preferred transcript in variant details dock widget

src/GSvar/VariantDetailsDockWidget.cpp
@@ -733,22 +733,21 @@ void VariantDetailsDockWidget::initTranscriptDetails(const VariantList& vl, int 
 				break;
 			}
 		}
-		//use first high-impact transcript
-		if (trans_curr==-1)
+		//fallback: first best transcript
+		if (trans_curr==-1 && LoginManager::active())
 		{
+			NGSD db;
 			for (int i=0; i<trans_data.count(); ++i)
 			{
-				if (trans_data[i].impact=="HIGH")
+				int gene_id = db.geneId(trans_data[i].gene);
+				if (gene_id==-1) continue;
+				Transcript best = db.bestTranscript(gene_id);
+				if (trans_data[i].id==best.nameWithVersion())
 				{
 					setTranscript(i);
 					break;
 				}
 			}
-		}
-		//fallback: first transcript
-		if (trans_curr==-1)
-		{
-			setTranscript(0);
 		}
 	}
 

commit ea1835c87692c3b56b4cc3440a9d03764ca41eab

GSvar: fixed minor bug introduced in 932bd4f896ede0b6ec8fa13e842d406cdbcfe862

src/GSvar/VariantDetailsDockWidget.cpp
@@ -749,6 +749,11 @@ void VariantDetailsDockWidget::initTranscriptDetails(const VariantList& vl, int 
 				}
 			}
 		}
+		//fallback: first transcript
+		if (trans_curr==-1)
+		{
+			setTranscript(0);
+		}
 	}
 
 	//tooltip if more than one transcript