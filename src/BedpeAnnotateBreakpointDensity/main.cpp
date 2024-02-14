4c6122c96667e332495199f265a08b8c1c4a2878
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2024-02-14 14:09:24 +0100

    src/NGSDExportSV/main.cpp: added export of processing system specific density files
    src/BedpeAnnotateBreakpointDensity/main.cpp: added option to annotate from processing system specific density files
    
    src/tools-TEST/*: added/updated test

--- src/BedpeAnnotateBreakpointDensity/main.cpp
+++ src/BedpeAnnotateBreakpointDensity/main.cpp
@@ -27,6 +27,7 @@ public:
 		//optional
 		addInfile("in", "Input BEDPE file. If unset, reads from STDIN.", true);
 		addOutfile("out", "Output BEDPE file. If unset, writes to STDOUT.", true);
+		addInfile("density_sys", "Optional IGV density file containing break point density for a specific processing system.", true);
 
 
 		changeLog(2022, 2, 23, "Initial commit.");
@@ -37,6 +38,8 @@ public:
 		//init
 		QString in = getInfile("in");
 		QString density_file_path = getInfile("density");
+		QString sys_density_file_path = getInfile("density_sys");
+		bool optional_sys_file = !sys_density_file_path.isEmpty();
 		QString out = getOutfile("out");
 		int idx_density_column = 1;
 
@@ -46,12 +49,23 @@ public:
 		if (!density_file.isSorted()) density_file.sort();
 		ChromosomalIndex<BedFile> anno_index(density_file);
 
+		//load processing system specific annotation file
+		BedFile sys_density_file;
+		if (optional_sys_file)
+		{
+			sys_density_file.load(sys_density_file_path);
+			if (!sys_density_file.isSorted()) sys_density_file.sort();
+		}
+		ChromosomalIndex<BedFile> sys_anno_index(sys_density_file);
+
 		//process BEDPE file
 		BedpeFile bedpe_file;
 		bedpe_file.load(in);
 
 		// check if annotation already exisits:
 		int i_annotation = bedpe_file.annotationIndexByName("NGSD_SV_BREAKPOINT_DENSITY", false);
+		int i_annotation_sys;
+		if (optional_sys_file) i_annotation_sys = bedpe_file.annotationIndexByName("NGSD_SV_BREAKPOINT_DENSITY_SYS", false);
 
 
 		// create output buffer and copy comments and header
@@ -61,6 +75,7 @@ public:
 		QByteArrayList header = bedpe_file.annotationHeaders();
 		// modify header if gene columns not already present
 		if (i_annotation < 0) header.append("NGSD_SV_BREAKPOINT_DENSITY");
+		if (optional_sys_file && i_annotation_sys < 0) header.append("NGSD_SV_BREAKPOINT_DENSITY_SYS");
 		// copy header
 		output_buffer << "#CHROM_A\tSTART_A\tEND_A\tCHROM_B\tSTART_B\tEND_B\t" + header.join("\t");
 
@@ -70,6 +85,7 @@ public:
 			BedpeLine line = bedpe_file[i];
 
 			QByteArrayList density_annotation;
+			QByteArrayList density_annotation_sys;
 
 
 			// determine density for first break point
@@ -96,6 +112,33 @@ public:
 				density_annotation << QByteArray::number((*std::max_element(density_bp2.begin(), density_bp2.end())));
 			}
 
+			if (optional_sys_file)
+			{
+				// determine density for first break point
+				QVector<int> density_bp1;
+				QVector<int> indices = sys_anno_index.matchingIndices(line.chr1(), line.start1() + 1, line.end1() + 1);
+				//get all density values
+				foreach(int index, indices)
+				{
+					density_bp1 << Helper::toInt(sys_density_file[index].annotations()[idx_density_column], "SV break point density (BP1, sys_specific)", QByteArray::number(i));
+				}
+				density_annotation_sys << QByteArray::number((*std::max_element(density_bp1.begin(), density_bp1.end())));
+
+				//determine density for second break point
+				//(insertions only have 1 breakpoint)
+				if (line.type() != StructuralVariantType::INS)
+				{
+					QVector<int> density_bp2;
+					indices = sys_anno_index.matchingIndices(line.chr2(), line.start2() + 1, line.end2() + 1);
+					//get all density values
+					foreach(int index, indices)
+					{
+						density_bp2 << Helper::toInt(sys_density_file[index].annotations()[idx_density_column], "SV break point density (BP2, sys_specific)", QByteArray::number(i));
+					}
+					density_annotation_sys << QByteArray::number((*std::max_element(density_bp2.begin(), density_bp2.end())));
+				}
+			}
+
 			//add annotation
 			QList<QByteArray> annotations = line.annotations();
 			if (i_annotation > -1)
@@ -105,6 +148,18 @@ public:
 			else
 			{
 				annotations.append(density_annotation.join(" / "));
+			}
+			if (optional_sys_file)
+			{
+				if (i_annotation_sys > -1)
+				{
+					annotations[i_annotation_sys] = density_annotation_sys.join(" / ");
+				}
+				else
+				{
+					annotations.append(density_annotation_sys.join(" / "));
+				}
+
 			}
 			line.setAnnotations(annotations);
 
--- src/tools-TEST/NGSDExportSV_Test.h
+++ src/tools-TEST/NGSDExportSV_Test.h
@@ -17,6 +17,7 @@ private slots:
 		db.executeQueriesFromFile(TESTDATA("data_in/NGSDExportSV_init1.sql"));
 
 		//create output folder
+		if (QDir("./out/NGSDExportSV").exists()) QDir("./out/NGSDExportSV").removeRecursively();
 		QDir(".").mkdir("out/NGSDExportSV");
 
 		//test
@@ -32,6 +33,37 @@ private slots:
 		COMPARE_FILES("out/NGSDExportSV/sv_inversion.bedpe", TESTDATA("data_out/NGSDExportSV/sv_inversion.bedpe"));
 		COMPARE_FILES("out/NGSDExportSV/sv_translocation.bedpe", TESTDATA("data_out/NGSDExportSV/sv_translocation.bedpe"));
 		COMPARE_FILES("out/NGSDExportSV/sv_breakpoint_density.igv", TESTDATA("data_out/NGSDExportSV/sv_breakpoint_density.igv"));
+		IS_FALSE(QFile::exists("out/NGSDExportSV/sv_breakpoint_density_hpHBOCv5.igv"));
+
+	}
+
+	void test_with_system_specific_files()
+	{
+		if (!NGSD::isAvailable(true)) SKIP("Test needs access to the NGSD test database!");
+
+		//init
+		NGSD db(true);
+		db.init();
+		db.executeQueriesFromFile(TESTDATA("data_in/NGSDExportSV_init1.sql"));
+
+		//delete previous exports and create output folder
+		if (QDir("./out/NGSDExportSV2").exists()) QDir("./out/NGSDExportSV2").removeRecursively();
+		QDir(".").mkdir("out/NGSDExportSV2");
+
+		//test
+		EXECUTE("NGSDExportSV", "-test -out_folder out/NGSDExportSV2 -common_sys_threshold 3");
+		REMOVE_LINES("out/NGSDExportSV2/sv_deletion.bedpe", QRegExp("##fileDate="));
+		REMOVE_LINES("out/NGSDExportSV2/sv_duplication.bedpe", QRegExp("##fileDate="));
+		REMOVE_LINES("out/NGSDExportSV2/sv_insertion.bedpe", QRegExp("##fileDate="));
+		REMOVE_LINES("out/NGSDExportSV2/sv_inversion.bedpe", QRegExp("##fileDate="));
+		REMOVE_LINES("out/NGSDExportSV2/sv_translocation.bedpe", QRegExp("##fileDate="));
+		COMPARE_FILES("out/NGSDExportSV2/sv_deletion.bedpe", TESTDATA("data_out/NGSDExportSV/sv_deletion.bedpe"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_duplication.bedpe", TESTDATA("data_out/NGSDExportSV/sv_duplication.bedpe"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_insertion.bedpe", TESTDATA("data_out/NGSDExportSV/sv_insertion.bedpe"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_inversion.bedpe", TESTDATA("data_out/NGSDExportSV/sv_inversion.bedpe"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_translocation.bedpe", TESTDATA("data_out/NGSDExportSV/sv_translocation.bedpe"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_breakpoint_density.igv", TESTDATA("data_out/NGSDExportSV/sv_breakpoint_density.igv"));
+		COMPARE_FILES("out/NGSDExportSV2/sv_breakpoint_density_hpHBOCv5.igv", TESTDATA("data_out/NGSDExportSV/sv_breakpoint_density_hpHBOCv5.igv"));
 	}
 
 
