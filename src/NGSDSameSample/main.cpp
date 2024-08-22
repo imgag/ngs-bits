893a39a4b49ff4b9999924b9b8c5be14dbc21944
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2024-08-22 13:44:38 +0200

    NGSDSameSample: fixed column order; ignoring bad samples by default now

--- src/NGSDSameSample/main.cpp
+++ src/NGSDSameSample/main.cpp
@@ -24,11 +24,13 @@ public:
 		addString("system_type", "Comma-separated list of processing system types.", true, "");
 		addString("system", "Comma-separated list of processing system (short) names.", true, "");
 		addEnum("mode", "Type of relation (either only same-sample or same-patient (includes same-sample).", true, QStringList() << "SAME_SAMPLE" << "SAME_PATIENT", "SAME_PATIENT");
-		addFlag("include_merged", "Include merged samples in the output (will be ignored on default).");
+		addFlag("include_bad", "Include bad processed samples in the output (will be ignored on default).");
+		addFlag("include_merged", "Include merged quality processed samples in the output (will be ignored on default).");
 		addFlag("test", "Uses the test database instead of on the production database.");
 
+		changeLog(2024,  8, 22, "remove bad samples by default");
+		changeLog(2023, 11, 21, "remove merged samples by default");
 		changeLog(2023, 11, 15, "initial commit");
-		changeLog(2023, 11, 21, "remove merged sample on default");
 	}
 
 	virtual void main()
@@ -70,8 +72,7 @@ public:
 		//get same samples
 		SameSampleMode mode = (getEnum("mode")=="SAME_PATIENT") ? SameSampleMode::SAME_PATIENT : SameSampleMode::SAME_SAMPLE;
 		QSet<int> same_samples = db.sameSamples(provided_s_id, mode);
-		// add provided sample id itself to report different processings
-		same_samples.insert(provided_s_id);
+		same_samples.insert(provided_s_id); // add provided sample id itself to report different processings
 
 		//get processed samples
 		QStringList ps_table;
@@ -81,29 +82,41 @@ public:
 			QList<int> ps_ids = db.getValuesInt("SELECT id FROM processed_sample WHERE sample_id=:0", QString::number(s_id));
 			foreach (int ps_id, ps_ids)
 			{
-				//skip sample itself:
+				//skip sample itself
 				if (ps_id == provided_ps_id) continue;
-				ProcessedSampleData ps_data = db.getProcessedSampleData(QString::number(ps_id));
-				QDate run_start_date = db.getValue("SELECT start_date FROM sequencing_run WHERE name=:0", false, ps_data.run_name).toDate();
-				QString sys_name_short = db.getValue("SELECT name_short FROM processing_system WHERE name_manufacturer=:0", false, ps_data.processing_system).toString();
+
+				//skip merged
 				if (!getFlag("include_merged"))
 				{
 					if (db.getValue("SELECT COUNT(processed_sample_id) FROM merged_processed_samples WHERE processed_sample_id=:0", false, QString::number(ps_id)).toInt() > 0) continue;
 				}
 
+				//skip bad quality
+				ProcessedSampleData ps_data = db.getProcessedSampleData(QString::number(ps_id));
+				if (!getFlag("include_bad"))
+				{
+					if (ps_data.quality=="bad") continue;
+				}
 
-				//apply filter
+				//apply filter (sample type)
 				if (!filter_sample_types.isEmpty() && !filter_sample_types.contains(s_data.type)) continue;
+
+				//apply filter (system type)
 				if (!filter_system_types.isEmpty() && !filter_system_types.contains(ps_data.processing_system_type)) continue;
+
+				//apply filter (system name)
+				QString sys_name_short = db.getValue("SELECT name_short FROM processing_system WHERE name_manufacturer=:0", false, ps_data.processing_system).toString();
 				if (!filter_systems.isEmpty() && !filter_systems.contains(sys_name_short)) continue;
 
+				//create output
 				QStringList line;
 				line << ps_data.name;
 				line << s_data.type;
-				line << sys_name_short;
 				line << ps_data.processing_system_type;
+				line << sys_name_short;
 				line << ps_data.processing_system;
 				line << ps_data.run_name;
+				QDate run_start_date = db.getValue("SELECT start_date FROM sequencing_run WHERE name=:0", false, ps_data.run_name).toDate();
 				line << run_start_date.toString("dd.MM.yyyy");
 
 				ps_table << line.join("\t");
