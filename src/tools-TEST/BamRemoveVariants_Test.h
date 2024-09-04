8e9265297e6cac066944aae553c5e7671593ec5a
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2024-09-04 11:12:33 +0200

    src/tools-TEST/*: added test for BamRemoveVariants

---
+++ src/tools-TEST/BamRemoveVariants_Test.h
@@ -0,0 +1,17 @@
+#include "TestFramework.h"
+
+
+TEST_CLASS(BamRemoveVariants_Test)
+{
+Q_OBJECT
+private slots:
+	
+	void default_parameters()
+	{
+		EXECUTE("BamRemoveVariants", "-in " + TESTDATA("data_in/BamRemoveVariants_in1.bam") + " -vcf " + TESTDATA("data_in/BamRemoveVariants_in1.vcf.gz")
+				+ " -out out/BamRemoveVariants_out1.bam");
+		COMPARE_GZ_FILES("out/BamRemoveVariants_out1.bam", TESTDATA("data_out/BamRemoveVariants_out1.bam"));
+	}
+	
+};
+
