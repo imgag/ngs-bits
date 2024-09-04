344d8346cb914f7de040ec98ca8c0371190ae4a4
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2024-09-04 10:12:30 +0200

    SomaticReport: Add result sentence if HLA-A*02:01 is hit by CNV

--- src/cppNGSD/SomaticReportHelper.cpp
+++ src/cppNGSD/SomaticReportHelper.cpp
@@ -1493,8 +1493,8 @@ RtfTable SomaticReportHelper::hlaTable(QString ps_tumor, QString ps_normal)
 
 		table.addDataRow({gene, normal_hla_allel1, normal_hla_allel2, tumor_hla_allel1, tumor_hla_allel2}, {1522, 2100, 2100, 2100, 2100});
 	}
-
 	table.setUniqueBorder(1,"brdrhair",4);
+
 	return table;
 }
 
@@ -2074,7 +2074,46 @@ RtfSourceCode SomaticReportHelper::partRelevantVariants()
 	QByteArray tumor_hla_allel2 = tumor_hla.isValid() ? tumor_hla.getGeneAllele("HLA-A", false) : "nicht bestimmbar";
 	if (normal_hla_allel1 == "HLA-A*02:01" || normal_hla_allel2 == "HLA-A*02:01" || tumor_hla_allel1 == "HLA-A*02:01" || tumor_hla_allel2 == "HLA-A*02:01" ) // CHECK HLA
 	{
-		out << RtfParagraph("Unsere Daten weisen auf das Vorliegen des Haplotyps HLA-A*02:01 in Tumor und Normalgewebe hin (s. Anlage).").setFontSize(18).setIndent(0,0,0).setSpaceAfter(30).setSpaceBefore(30).setHorizontalAlignment("j").setLineSpacing(276).RtfCode();
+		RtfSourceCode desc;
+
+		desc += RtfText("Unsere Daten weisen auf das Vorliegen des Haplotyps HLA-A*02:01 in Tumor- und Normalprobe hin (s. Anlage).").RtfCode();
+
+		//check for CNVs in the HLA gene if the allele is HLA-A*02:01
+		if (tumor_hla.isValid() && (tumor_hla.getGeneAllele("HLA-A", true) == "HLA-A*02:01" || tumor_hla.getGeneAllele("HLA-A", false) == "HLA-A*02:01"))
+		{
+			QByteArray cnv_type = "";
+			for(int i=0;i<cnvs_.count();++i)
+			{
+				CopyNumberVariant cnv = cnvs_[i];
+				GeneSet genes = cnv.genes();
+
+				if (genes.contains("HLA-A"))
+				{
+					cnv_type = CnvTypeDescription(cnv.copyNumber(cnvs_.annotationHeaders()), true).replace('(', "").replace(')', "");
+					break;
+				}
+
+			}
+
+			if (cnv_type != "")
+			{
+				double depth_allel1 = tumor_hla.getGeneAlleleDepth("HLA-A", true);
+				double depth_allel2 = tumor_hla.getGeneAlleleDepth("HLA-A", false);
+
+
+				double rel_depth1 = depth_allel1 / (depth_allel1 + depth_allel2);
+				double rel_depth2 = depth_allel2 / (depth_allel1 + depth_allel2);
+
+
+
+				desc += RtfText(" Es liegt eine Kopienzahlveränderung (" + cnv_type + ") im HLA-A Genlokus der Tumorprobe vor. ").RtfCode();
+				desc += RtfText("Das Allel A*02:01 ist im Tumorgewebe " + RtfText("deletiert amplifiziert").highlight(3).RtfCode()+ ". " + RtfText("("+ tumor_hla_allel1 + " Freq " + QByteArray::number(rel_depth1, 'f', 2) + " | " + tumor_hla_allel2 + " Freq " + QByteArray::number(rel_depth2, 'f', 2) + ") ").highlight(4).RtfCode()).RtfCode();
+				desc += RtfText("Ein Verlust des Allels A*02:01 im Tumorgewebe ist nicht beurteilbar.").highlight(3).RtfCode();
+
+			}
+		}
+
+		out << RtfParagraph(desc).setFontSize(18).setIndent(0,0,0).setSpaceAfter(30).setSpaceBefore(30).setHorizontalAlignment("j").setLineSpacing(276).RtfCode();
 	}
 	else
 	{
