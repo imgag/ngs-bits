f6e63cbe1b94df9e424b3f595e412c002e6150df
KilianIlius <77616357+KilianIlius@users.noreply.github.com>
2024-11-28 15:47:24 +0100

    Handling of overlapping pathogenic CNV in NGSDAnnotateCNV (#602)
    Co-authored-by: Kilian Ilius <kilian.ilius@med.uni-tuebingen.de>

--- src/NGSDAnnotateCNV/main.cpp
+++ src/NGSDAnnotateCNV/main.cpp
@@ -5,10 +5,17 @@
 #include "BedpeFile.h"
 #include "TSVFileStream.h"
 
-bool cnv_class_rev_sort(QPair<int, double> i, QPair<int, double> j)
+struct PathogenicCnv
 {
-		if (i.first == j.first) return i.second > j.second;
-		return i.first > j.first;
+	int p_class;
+	double p_cnv_overlap;
+	double cnv_overlap;
+	bool annotate;
+};
+
+bool cnv_class_rev_sort(const PathogenicCnv& i, const PathogenicCnv& j) {
+	if (i.p_class == j.p_class) return i.p_cnv_overlap > j.p_cnv_overlap;
+	return i.p_class > j.p_class;
 }
 
 class ConcreteTool
@@ -46,8 +53,6 @@ public:
 		SqlQuery sql_query = db.getQuery();
 		sql_query.prepare("SELECT rcc.class, cnv.start, cnv.end FROM cnv INNER JOIN report_configuration_cnv rcc ON cnv.id = rcc.cnv_id WHERE rcc.class IN ('4', '5') AND cnv.chr = :0 AND cnv.start <= :1 AND :2 <= cnv.end ");
 
-
-
 		out << "annotate TSV file..." << endl;
 
 		// copy comments
@@ -74,12 +79,13 @@ public:
 		{
 			// read next line:
 			QByteArrayList tsv_line = cnv_input_file.readLine();
-			QList<QPair<int,double>> pathogenic_cnvs;
+			QList<PathogenicCnv> pathogenic_cnvs;
 
 			// parse position:
 			Chromosome chr = Chromosome(tsv_line[i_chr]);
 			int start = Helper::toInt(tsv_line[i_start], "start");
 			int end = Helper::toInt(tsv_line[i_end], "end");
+			int cnv_length = end - start;
 
 			// get all overlaping CNVs
 			sql_query.bindValue(0, chr.strNormalized(true));
@@ -88,17 +94,37 @@ public:
 			sql_query.exec();
 			while(sql_query.next())
 			{
-				int p_class = sql_query.value(0).toInt();
+				PathogenicCnv p_cnv_stats;
+				p_cnv_stats.p_class = sql_query.value(0).toInt();
 				int p_start = sql_query.value(1).toInt();
 				int p_end = sql_query.value(2).toInt();
 
-				// compute overlap
+				// compute overlaps
 				int p_cnv_length = p_end - p_start;
 				int intersection = std::min(p_end, end) - std::max(p_start, start);
-				double overlap = (double) intersection / p_cnv_length;
+				p_cnv_stats.p_cnv_overlap = (double) intersection / p_cnv_length;
+				p_cnv_stats.cnv_overlap = (double) intersection / cnv_length;
 
-				// store tuple
-				pathogenic_cnvs.append(QPair<int, double>(p_class, overlap));
+				// check if pathogenic cnv should be annotated
+				/// pathogenic CNV fully contained within current CNV
+				if (p_start >= start && p_end <= end)
+				{
+					p_cnv_stats.annotate = true;
+				}
+				/// at least 10% of the pathogenic CNV overlaps with at least 10% of the current CNV
+				else if (p_cnv_stats.p_cnv_overlap >= 0.1 && p_cnv_stats.cnv_overlap >= 0.1)
+				{
+					p_cnv_stats.annotate = true;
+				}
+				/// current CNV is fully contained within the pathogenic CNV and represents at least 10% of the pathogenic CNV
+				else if ((start >= p_start && end <= p_end) && p_cnv_stats.p_cnv_overlap >= 0.1)
+				{
+					p_cnv_stats.annotate = true;
+				}
+				else p_cnv_stats.annotate = false;
+
+				// store cnv_stats
+				pathogenic_cnvs.append(p_cnv_stats);
 
 			}
 
@@ -107,12 +133,14 @@ public:
 
 			// convert to QByteArray
 			QByteArrayList pathogenic_cnv_entries;
-			QPair<int,double> p_cnv;
+			PathogenicCnv p_cnv;
 			foreach(p_cnv, pathogenic_cnvs)
 			{
-				pathogenic_cnv_entries.append(QByteArray::number(p_cnv.first) + "/" + QByteArray::number(std::max(p_cnv.second, 0.0001), 'f', 4));
+				if (p_cnv.annotate)
+				{
+					pathogenic_cnv_entries.append(QByteArray::number(p_cnv.p_class) + "/" + QByteArray::number(p_cnv.p_cnv_overlap, 'f', 3));
+				}
 			}
-
 
 			// update annotation
 			if (i_path_cnvs < 0) tsv_line.append(pathogenic_cnv_entries.join(" "));
@@ -137,7 +165,7 @@ public:
 
 		out << "annotation complete (runtime: " << Helper::elapsedTime(timer) << ")." << endl;
 
-	}	
+	}
 };
 
 #include "main.moc"
