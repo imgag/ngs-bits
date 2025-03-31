5094452f94c12faf4eafb2e12951ceb0f74645d0
AHIliuK1 <ahiliuk1@SRV0R26.img.med.uni-tuebingen.de>
2025-03-31 15:23:17 +0200

    GSVarServer: somatic pipeline split

--- src/GSvarServer/SgeStatusUpdateWorker.cpp
+++ src/GSvarServer/SgeStatusUpdateWorker.cpp
@@ -240,7 +240,7 @@ void SgeStatusUpdateWorker::startAnalysis(NGSD& db, const AnalysisJob& job, int 
 		}
 		else
 		{
-			out_folder = project_folder + "/Sample_" + ps_data[t_idx].name + "/";
+			out_folder = project_folder + "/Somatic_" + ps_data[t_idx].name + "/";
 		}
 		if (Helper::mkdir(out_folder)==1)
 		{
@@ -251,7 +251,7 @@ void SgeStatusUpdateWorker::startAnalysis(NGSD& db, const AnalysisJob& job, int 
 			}
 		}
 
-		script = "somatic_dna.php";
+		script = (n_idx!=-1) ? "somatic_tumor_normal.php" : "somatic_tumor_only.php";
 		args << "--log "+out_folder+"/somatic_dna_"+timestamp+".log";
 		args << "-out_folder " + out_folder;
 		args << "-prefix " + ps_data[t_idx].name + (n_idx!=-1 ? "-"+ps_data[n_idx].name : "");
