b77878d34a590ef5d1e158468f14076aa13e751b
KilianIlius <kilian.ilius@ilmacom.de>
2025-04-03 15:59:36 +0200

    GSVarServer: openProcessedSampleFromNGSD added tumor-only filepath

--- src/GSvarServer/SgeStatusUpdateWorker.cpp
+++ src/GSvarServer/SgeStatusUpdateWorker.cpp
@@ -252,7 +252,7 @@ void SgeStatusUpdateWorker::startAnalysis(NGSD& db, const AnalysisJob& job, int 
 		}
 
 		script = (n_idx!=-1) ? "somatic_tumor_normal.php" : "somatic_tumor_only.php";
-		args << "--log "+out_folder+"/somatic_dna_"+timestamp+".log";
+		args << "--log "+out_folder+"/somatic_"+timestamp+".log";
 		args << "-out_folder " + out_folder;
 		args << "-prefix " + ps_data[t_idx].name + (n_idx!=-1 ? "-"+ps_data[n_idx].name : "");
 		args << "-t_bam " + bams[t_idx];
