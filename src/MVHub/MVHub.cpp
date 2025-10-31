9a8800705698fe922d642f4c40d2170817e95c87
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2025-10-31 10:46:35 +0100

    fixed Qt6 error

--- src/MVHub/MVHub.cpp
+++ src/MVHub/MVHub.cpp
@@ -575,7 +575,7 @@ void MVHub::updateTableFilters()
 		int c_ps = colOf("PS");
 		int c_ps_tumor = colOf("PS tumor");
 
-		QSet<QString> exclude = f_exclude_ps.split(' ').toSet();
+		QSet<QString> exclude = LIST_TO_SET(f_exclude_ps.split(' '));
 		for (int r=0; r<rows; ++r)
 		{
 			if (!visible[r]) continue;
