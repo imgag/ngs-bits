a078a6d0b4d47452f638944eabe0b6e1152be579
KilianIlius <kilian.ilius@ilmacom.de>
2026-01-07 16:38:57 +0100

    Fix VcfPruneSourceVariant test

--- src/tools-TEST/VcfPruneSourceVariant_Test.cpp
+++ src/tools-TEST/VcfPruneSourceVariant_Test.cpp
@@ -1,4 +1,5 @@
-#include "TestFramework.h"
+#include "TestFrameworkNGS.h"
+#include "Settings.h"
 
 TEST_CLASS(VcfPruneSourceVariant_Test)
 {
@@ -6,7 +7,11 @@ private:
 
 	TEST_METHOD(default_mode)
 	{
-		EXECUTE("VcfPruneSourceVariant", "-in " + TESTDATA("data_in/VcfPruneSourceVariant_in1.vcf") + " -out out/VcfPruneSourceVariant_out1.vcf");
+		SKIP_IF_NO_HG38_GENOME();
+
+		QString ref_file = Settings::string("reference_genome", true);
+
+		EXECUTE("VcfPruneSourceVariant", "-in " + TESTDATA("data_in/VcfPruneSourceVariant_in1.vcf") + " -out out/VcfPruneSourceVariant_out1.vcf -ref " + ref_file);
 		COMPARE_FILES("out/VcfPruneSourceVariant_out1.vcf", TESTDATA("data_out/VcfPruneSourceVariant_out1.vcf"));
 	}
 
