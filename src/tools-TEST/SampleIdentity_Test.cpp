8a9aafc80fd9865e9c2b5eb4ea6d43034135882d
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2026-01-30 14:34:24 +0100

    bug fix

--- src/tools-TEST/SampleIdentity_Test.cpp
+++ src/tools-TEST/SampleIdentity_Test.cpp
@@ -1,4 +1,4 @@
-#include "TestFramework.h"
+#include "TestFrameworkNGS.h"
 
 TEST_CLASS(SampleIdentity_Test)
 {
@@ -6,6 +6,8 @@ private:
 	
 	TEST_METHOD(wes_rna_test)
 	{
+	  SKIP_IF_NO_HG38_GENOME();
+
 		EXECUTE("SampleIdentity", "-bams " + TESTDATA("data_in/SampleIdentity_in_wes.cram") + " " + TESTDATA("data_in/SampleIdentity_in_rna.cram") + " -out out/SampleIdentity_out1.tsv -basename");
 		COMPARE_FILES("out/SampleIdentity_out1.tsv", TESTDATA("data_out/SampleIdentity_out1.tsv"));
 	}
