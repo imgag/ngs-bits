19f808c8f52237f47c5b48d39b159a7e0806729a
Rajinish Aneel <rajinishaneel@gmail.com>
2026-02-27 12:09:41 +0100

    use for loop for tests

--- src/tools-TEST/SampleIdentity_Test.cpp
+++ src/tools-TEST/SampleIdentity_Test.cpp
@@ -6,25 +6,12 @@ private:
 	
 	TEST_METHOD(wes_rna_test_1_thread)
 	{
-	  SKIP_IF_NO_HG38_GENOME();
-
-		EXECUTE("SampleIdentity", "-bams " + TESTDATA("data_in/SampleIdentity_in_wes.cram") + " " + TESTDATA("data_in/SampleIdentity_in_rna.cram") + " -out out/SampleIdentity_out1.tsv -basename -min_snps 30 -threads 1");
-		COMPARE_FILES("out/SampleIdentity_out1.tsv", TESTDATA("data_out/SampleIdentity_out1.tsv"));
-	}
-
-	TEST_METHOD(wes_rna_test_2_threads)
-	{
 		SKIP_IF_NO_HG38_GENOME();
 
-		EXECUTE("SampleIdentity", "-bams " + TESTDATA("data_in/SampleIdentity_in_wes.cram") + " " + TESTDATA("data_in/SampleIdentity_in_rna.cram") + " -out out/SampleIdentity_out1.tsv -basename -min_snps 30 -threads 2");
-		COMPARE_FILES("out/SampleIdentity_out1.tsv", TESTDATA("data_out/SampleIdentity_out1.tsv"));
-	}
-
-	TEST_METHOD(wes_rna_test_3_threads)
-	{
-		SKIP_IF_NO_HG38_GENOME();
-
-		EXECUTE("SampleIdentity", "-bams " + TESTDATA("data_in/SampleIdentity_in_wes.cram") + " " + TESTDATA("data_in/SampleIdentity_in_rna.cram") + " -out out/SampleIdentity_out1.tsv -basename -min_snps 30 -threads 3");
-		COMPARE_FILES("out/SampleIdentity_out1.tsv", TESTDATA("data_out/SampleIdentity_out1.tsv"));
+		foreach(QString threads, QStringList() << "1" << "2" << "4")
+		{
+			EXECUTE("SampleIdentity", "-bams " + TESTDATA("data_in/SampleIdentity_in_wes.cram") + " " + TESTDATA("data_in/SampleIdentity_in_rna.cram") + " -out out/SampleIdentity_out1.tsv -basename -min_snps 30 -threads "+threads);
+			COMPARE_FILES("out/SampleIdentity_out1.tsv", TESTDATA("data_out/SampleIdentity_out1.tsv"));
+		}
 	}
 };
