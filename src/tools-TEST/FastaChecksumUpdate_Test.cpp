f9cf5b867a9e08940574795994646bf0f3c5eaae
Rajinish Aneel <rajinishaneel@gmail.com>
2026-03-02 14:23:06 +0100

    update test name

--- src/tools-TEST/FastaChecksumUpdate_Test.cpp
+++ src/tools-TEST/FastaChecksumUpdate_Test.cpp
@@ -3,7 +3,7 @@
 TEST_CLASS(FastaChecksumUpdate_Test)
 {
 private:
-	TEST_METHOD(cheksum_update_sum)
+	TEST_METHOD(cheksum_update_test)
 	{
 		EXECUTE("FastaChecksumUpdate", "-in " + TESTDATA("data_in/FastaChecksumUpdate_in1.fa") + " -out out/FastaChecksumUpdate_out1.fa");
 		COMPARE_FILES("out/FastaChecksumUpdate_out1.fa", TESTDATA("data_out/FastaChecksumUpdate_out1.fa"));
