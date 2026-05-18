2da32c11bbb74f15c8a79cbfb4c06cbff1c1a6f9
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2026-05-18 09:09:16 +0200

    src/GSvar/BurdenTestWidget.cpp: get CNP regions from  different settings entry
    bin/GSvar.ini.example: get CNP regions from  different settings entry

--- src/GSvar/BurdenTestWidget.cpp
+++ src/GSvar/BurdenTestWidget.cpp
@@ -999,16 +999,11 @@ void BurdenTestWidget::performBurdenTest()
 		}
 
 		//read polymorphism region
-		QStringList igv_tracks = Settings::stringList("igv_menu"); //TODO Leon: seperate entry!
-		foreach (const QString& track, igv_tracks)
-		{
-			QStringList columns = track.split('\t');
-			if (columns.at(0).startsWith("Copy-number polymorphism regions"))
-			{
-				cnv_polymorphism_region.load(columns.at(2).trimmed());
-			}
-		}
-
+		cnv_polymorphism_region.load(Settings::string("burden_test_cnp_regions"));
+		cnv_polymorphism_region.sort();
+		qDebug() << "path:" << Settings::string("burden_test_cnp_regions");
+		qDebug() << "Base count:" << cnv_polymorphism_region.baseCount();
+		qDebug() << "is sorted:" << cnv_polymorphism_region.isMergedAndSorted();
 	}
 	ChromosomalIndex<BedFile> cnv_polymorphism_region_index(cnv_polymorphism_region);
 
