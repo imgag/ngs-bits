f592a7b49b6b23e2ecf39cd9345d0a74d891aeb2
Alexander Ott <Alexander.Ott@med.uni-tuebingen.de>
2026-07-28 10:08:40 +0200

    allow translation of SAP_IDs to patient_id and processed_sample_names

--- src/GenlabInfo/main.cpp
+++ src/GenlabInfo/main.cpp
@@ -16,7 +16,8 @@ public:
 	virtual void setup()
 	{
 		setDescription("Provide sample information from GenLAB.");
-		addString("ps", "Processed sample or TSV file with processed sample ids in the first column", false);
+		addString("ps", "Processed sample or TSV file with processed sample ids in the first column", true, "");
+		addString("sap_id", "SAP id or TSV file with SAP ids in the first column. Will provide a table with patient ID and processed samples names.", true, "");
 		//optional
 		addString("info", "Infos that will be collected from Genlab.Comma seperated list of values. Supported: SAPID,PATID", true, "SAPID");
 		addString("out", "TSV file where the Genlab infos will be written to. stdout if emtpy.", true);
@@ -29,31 +30,86 @@ public:
 		GenLabDB genlab;
 
 		QString ps = getString("ps");
-		QStringList infos = getString("info").split(",");
-		QSharedPointer<QFile> out = Helper::openFileForWriting(getString("out"), true);
+		QString sap_id = getString("sap_id");
 
-		QStringList out_header;
-		out_header << "#sample" << infos;
-		out->write(out_header.join("\t").toUtf8() +  "\n");
+		if (ps.isEmpty() && sap_id.isEmpty()) THROW(ArgumentException, "Either a processed sample or a SAP id has to be provided.");
+		if (! ps.isEmpty() && ! sap_id.isEmpty()) THROW(ArgumentException, "A processed sample and a SAP id cannot be procesed at the same time. Please call the tool with only one of the two.");
 
-		QStringList ps_names;
-		if (! QFileInfo(ps).isFile())
+		//Procesed Sample to SAP ID or patient ID
+		if (!ps.isEmpty())
 		{
-			ps_names << ps;
-		}
-		else
-		{
-			TsvFile in_file;
-			in_file.load(ps);
-			ps_names = in_file.extractColumn(0);
+			QStringList infos = getString("info").split(",");
+			QSharedPointer<QFile> out = Helper::openFileForWriting(getString("out"), true);
+
+			QStringList out_header;
+			out_header << "#sample" << infos;
+			out->write(out_header.join("\t").toUtf8() +  "\n");
+
+			QStringList ps_names;
+			if (! QFileInfo(ps).isFile())
+			{
+				ps_names << ps;
+			}
+			else
+			{
+				TsvFile in_file;
+				in_file.load(ps);
+				ps_names = in_file.extractColumn(0);
+			}
+
+			foreach(const QString& ps_name, ps_names)
+			{
+				out->write(ps_name.toUtf8() + "\t" + getInfos(ps_name.trimmed(), genlab, infos).join("\t").toUtf8() + "\n");
+			}
+
+			out->close();
 		}
 
-		foreach(const QString& ps_name, ps_names)
+		//SAP ID to patient id and processed samples
+		if (!sap_id.isEmpty())
 		{
-			out->write(ps_name.toUtf8() + "\t" + getInfos(ps_name.trimmed(), genlab, infos).join("\t").toUtf8() + "\n");
+			QSharedPointer<QFile> out = Helper::openFileForWriting(getString("out"), true);
+
+			QStringList out_header;
+			out_header << "#sap_id" << "patient_id" << "processed_samples";
+			out->write(out_header.join("\t").toUtf8() +  "\n");
+
+			QStringList sap_ids;
+			if (! QFileInfo(sap_id).isFile())
+			{
+				sap_ids << sap_id;
+			}
+			else
+			{
+				TsvFile in_file;
+				in_file.load(sap_id);
+				sap_ids = in_file.extractColumn(0);
+			}
+
+			foreach(const QString& sap_id, sap_ids)
+			{
+				out->write(sap_id.toUtf8() + "\t" + getSapIdInfos(sap_id.trimmed(), genlab).join("\t").toUtf8() + "\n");
+			}
+			out->close();
+		}
+	}
+
+	QStringList getSapIdInfos(const QString& sap_id, GenLabDB& genlab)
+	{
+		//pat ID:
+
+		ProcessedSampleSearchParameters params;
+		QStringList ps_samples = genlab.samplesWithSapID(sap_id, params);
+		QSet<QString> pat_ids_set;
+		foreach(const QString& ps, ps_samples)
+		{
+			pat_ids_set << genlab.patientIdentifier(ps);
 		}
 
-		out->close();
+		QStringList infos;
+		infos << QStringList(pat_ids_set.begin(), pat_ids_set.end()).join(",");
+		infos << ps_samples.join(",");
+		return infos;
 	}
 
 	QStringList getInfos(const QString& ps, GenLabDB& genlab, const QStringList& infos)
@@ -73,6 +129,7 @@ public:
 		}
 		return ps_infos;
 	}
+
 };
 
 #include "main.moc"
