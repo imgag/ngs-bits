b26429a9758757c2846131c6e732643a102b4e9f
Alexandr Chernov <kstchernov@gmail.com>
2026-07-28 14:48:57 +0200

    Fixing broken tests

--- src/tools-TEST/GenlabInfo_Test.cpp
+++ src/tools-TEST/GenlabInfo_Test.cpp
@@ -23,15 +23,15 @@ private:
 	
 	TEST_METHOD(getInfoSapId)
 	{
+		SKIP_IF_NO_PROD_GENLAB();
+
 		//SAP ID to sample
 		EXECUTE("GenlabInfo", "-sap_id 4942684 -out out/GenlabInfo_out5.tsv");
 		COMPARE_FILES("out/GenlabInfo_out5.tsv", TESTDATA("data_out/GenlabInfo_out5.tsv"));
 
 		EXECUTE("GenlabInfo", "-sap_id " + TESTDATA("data_in/GenlabInfo_in2.tsv") + " -info SAPID,PATID -out out/GenlabInfo_out6.tsv");
-		COMPARE_FILES("out/GenlabInfo_out6.tsv", TESTDATA("data_out/GenlabInfo_out6.tsv"));
-		
+		COMPARE_FILES("out/GenlabInfo_out6.tsv", TESTDATA("data_out/GenlabInfo_out6.tsv"));		
 	}
-
 };
 
 
